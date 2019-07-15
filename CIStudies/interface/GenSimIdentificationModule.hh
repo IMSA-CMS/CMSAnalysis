8f1e88fb14e8facdd5c59467cd23d39db4e56fa4
Ayan Mallik <amallik@imsa.edu>
2019-07-15 09:54:31 -0500

    added src code and new production modules

--- CIStudies/interface/GenSimIdentificationModule.hh
+++ CIStudies/interface/GenSimIdentificationModule.hh
@@ -19,11 +19,17 @@ class GenSimIdentificationModule : public ProductionModule
 {
 public:
   virtual void process(const edm::EventBase& event) override;
+  bool isParticle(const reco::GenParticle& p) const;
 
   const std::vector<const reco::GenParticle*>& getGenParticles() const
   {return genParticles;}
+
 private:
   std::vector<const reco::GenParticle*> genParticles;
+  double ptCut;
+  bool isElectronIfTrue;
 };
 
 #endif
+
+
--- CIStudies/interface/MatchingModule.hh
+++ CIStudies/interface/MatchingModule.hh
@@ -3,14 +3,27 @@
 
 #include "ProductionModule.hh"
 
+#include <vector>
+
 class MatchingModule : public ProductionModule
 {
 public:
-  MatchingModule(const GenSimIdentificationModule& gensimModule);
+  MatchingModule(const GenSimIdentificationModule& genSimModule, const RecoIdentificationModuke& recoModule);
   virtual void process(const edm::EventBase& event) override;
+
+struct MatchedList
+  {
+    const reco::GenParticle* bestGenParticle;
+    const reco::RecoCandidate* bestRecoParticle;
+    double deltaR;
+    double phiError;
+    double etaError;
+    double ptError;
+  };
 
 private:
   const GenSimIdentificationModule& genSim;
+  const RecoIdentificationModule& reco;
 };
 
 #endif
