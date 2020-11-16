e4a55d57985d472031ebae6354621e8f2203c3cb
Kodai Speich <kspeich@imsa.edu>
2020-11-16 10:32:26 -0600

    Added MassRecoEfficiency

---
+++ CIStudies/interface/MassRecoEfficiency.hh
@@ -0,0 +1,23 @@
+#ifndef MASSRECOEFFICIENCY_HH
+#define MASSRECOEFFICIENCY_HH
+
+#include "AnalysisModule.hh"
+
+class RecoIdentificationModule;
+
+class MassRecoEfficiency : public AnalysisModule
+{
+public:
+  MassRecoEfficiency(const RecoIdentificationModule& iRecoModule, double iHiggsMass, double iWidth);
+  bool process(const edm::EventBase& event) override;
+  void finalize() override;
+  void writeAll() override {}
+private:
+  const RecoIdentificationModule& recoModule;
+  double HiggsMass;
+  double width;     // Checks HiggsMass +/- width
+  int passCount;
+  int totalCount;
+};
+
+#endif
