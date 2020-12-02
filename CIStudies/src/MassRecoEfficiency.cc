e8a228dff8f65c4957ec3117ac7100635d8ff871
Kodai Speich <kspeich@imsa.edu>
2020-12-01 20:35:54 -0600

    Updated MassRecoEfficiency

--- CIStudies/src/MassRecoEfficiency.cc
+++ CIStudies/src/MassRecoEfficiency.cc
@@ -15,6 +15,7 @@ bool MassRecoEfficiency::process(const edm::EventBase& event)
   auto reco = recoModule.getRecoCandidates();
   double invMass = reco.calculateSameSignInvariantMass();
   int size = reco.getNumParticles();
+  int nMuons = reco.getLeptonTypeCount(Particle::LeptonType::Muon);
 
   double min = HiggsMass - width;
   double max = HiggsMass + width;
@@ -31,15 +32,27 @@ bool MassRecoEfficiency::process(const edm::EventBase& event)
     nLeptonTotalCount.insert({size, 0});
   }
 
+  // Same thing with the other two
+  if (muonsNLeptonPassCount.find(size) == muonsNLeptonPassCount.end())
+  {
+    muonsNLeptonPassCount.insert({size, {{nMuons, 0}}});
+  }
+
+  if (muonsNLeptonTotalCount.find(size) == muonsNLeptonTotalCount.end())
+  {
+    muonsNLeptonTotalCount.insert({size, {{nMuons, 0}}});
+  }
+
   if (invMass > min && invMass < max)       // if min < invMass < max; in other words, the invariant mass is in the range we're checking
   {
     ++passCount;                            // Passes, increment the pass count
     ++nLeptonPassCount[size];               // Passes, increment the pass count for the count specific to the number of leptons
-    
+    ++muonsNLeptonPassCount[size][nMuons];
   }
 
   ++totalCount;
   ++nLeptonTotalCount[size];
+  ++muonsNLeptonTotalCount[size][nMuons];
 
   return true;
 }
@@ -48,11 +61,29 @@ void MassRecoEfficiency::finalize()
 {
   std::cout << "Mass Reconstruction Efficiency: " << passCount / (double) totalCount << std::endl;
 
+  std::cout << '\n';  // For sanity's sake
+
   for (auto totalCount : nLeptonTotalCount)
   {
     int pass = nLeptonPassCount[totalCount.first];
     int total = totalCount.second;
     std::cout << "Leptons: " << totalCount.first << "; Accepted: " << pass << "; Total: " << total << std::endl;
     std::cout << "Mass Reconstruction Efficiency for " << totalCount.first << " leptons: " << pass / (double) total << std::endl;
+  }
+
+  std::cout << '\n';  // Again, for sanity's sake
+
+  for (auto muonCountPairs : muonsNLeptonTotalCount)
+  {
+    for (auto totalCount : muonCountPairs.second)
+    {
+      int pass = muonsNLeptonPassCount[muonCountPairs.first][totalCount.first];
+      int total = totalCount.second;
+      std::cout << "Muons: " << totalCount.first << "; Leptons: " << muonCountPairs.first << "; Accepted: " << pass << "; Total: " << total << std::endl;
+      std::cout << "Mass Reconstruction Efficiency for " << totalCount.first << " muons out of " << muonCountPairs.first << " muons: " << pass / (double) total << std::endl;
+    }  
+
+  std::cout << '\n';  // Yet again, for sanity's sake
+
   }
 }
