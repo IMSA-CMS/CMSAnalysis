3a1eb728c684b9f415910c92f0dcbecf00bed0fc
ttchalakov <teodor.tch@gmail.com>
2021-02-21 17:23:54 -0600

    Formatted gensimeventdump to display all daughters and mothers

--- CIStudies/src/GenSimEventDumpModule.cc
+++ CIStudies/src/GenSimEventDumpModule.cc
@@ -37,6 +37,8 @@ void GenSimEventDumpModule::writeAll()
 void GenSimEventDumpModule::printGenParticleCollection(const reco::GenParticleCollection& genParts) const
 {
   int eventElement = 1;
+  int motherColumnWidth = 20;
+  int daughterColumnWidth = 20;
 
   // Format
   std::cerr << "--------------------------------------------------------" << std::endl;
@@ -44,47 +46,46 @@ void GenSimEventDumpModule::printGenParticleCollection(const reco::GenParticleCo
   std::cerr << "--------------------------------------------------------" << std::endl;
 
   std::cout << std::left << std::setw(8) << "element" << std::setw(11) << "| pdfId"
-   << std::setw(10) << "| status" << std::setw(10) << "| mother1"
-   << std::setw(10) << "| mother2" << std::setw(12) << "| daughter1"
-   << std::setw(12) << "| daughter2" << std::setw(15) << "| px"
-   << std::setw(15) << "| py" << std::setw(15) << "| pz"
-   << std::setw(15) << "| E" << std::setw(5) << "| mass\n";
+   << std::setw(10) << "| status"
+   << std::setw(motherColumnWidth) << "| mothers"
+   << std::setw(daughterColumnWidth) << "| daughters"
+   << std::setw(15) << "| px"
+   << std::setw(15) << "| py"
+   << std::setw(15) << "| pz"
+   << std::setw(15) << "| E"
+   << std::setw(5) << "| mass\n";
 
   //Prints out all of the particles
   for(auto &part : genParts){
     
     // Event Elements
-    int daughter1 = 0;
-    int daughter2 = 0;
+    std::string mothers = "";
+    std::string daughters = "";
 
-    int mother1 = 0;
-    int mother2 = 0;
-
-    // Distinguishing Mothers
-    if(part.numberOfMothers() != 0)
+    for(int i = 0; i < (int) part.numberOfMothers(); i++)
     {
-      mother1 = 1 + getIndexOf(part.mother(0), genParts);
-      mother2 = 1 + getLatestIndexOfMothers(part, genParts);
+      mothers = mothers + (1 + getIndexOf(part.mother(i), genParts)) + " ";
     }
 
     // Debug
     //std::cout << part.numberOfMothers() << ", " << part.numberOfDaughters() << std::endl;
-   
-    // Distinguishing Daughters
-    if(part.numberOfDaughters() != 0)
+
+    for(int i = 0; i < (int) part.numberOfDaughters(); i++)
     {
-      daughter1 = 1 + getIndexOf(part.daughter(0), genParts);
-      daughter2 = 1 + getLatestIndexOfDaughters(part, genParts);
+      daughters = daughters + (1 + getIndexOf(part.daughter(i), genParts)) + " ";
     }
+
+    // TODO: Write a method that formats daughter and mother output.
+    // From 1 2 3 4 7 to 1-4 7
 
     // Standard info
     std::cout << std::setw(8) << eventElement << "| " << std::setw(9) << part.pdgId() << "| " << std::setw(8) << part.status() << "| ";
 
     // Print mothers
-    std::cout << std::setw(8) << mother1 << "| " << std::setw(8) << mother2 << "| ";
+    std::cout << std::setw(motherColumnWidth - 2) << mothers << "| ";
 
     // Print daughters
-    std::cout << std::setw(10) << daughter1 << "| " << std::setw(10) << daughter2 << "| ";
+    std::cout << std::setw(daughterColumnWidth - 2) << daughters << "| ";
 
     // Particle properties
     std::cout << std::setw(13) << part.px() << "| " << std::setw(13) << part.py() << "| " << std::setw(13) << part.pz() << "| " << std::setw(13) << part.energy() << "| " << std::setw(13) << part.mass() << "\n";
