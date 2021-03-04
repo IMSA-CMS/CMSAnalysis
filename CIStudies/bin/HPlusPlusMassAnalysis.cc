2e2495037f9147adb38d5cf816c47f89953b0425
James Tan <jtan@imsa.edu>
2021-03-03 22:06:47 -0600

    Allow Histograms to Display

--- CIStudies/bin/HPlusPlusMassAnalysis.cc
+++ CIStudies/bin/HPlusPlusMassAnalysis.cc
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <memory>
 
 #include "TROOT.h"
 #include "TSystem.h"
 
 #include "CIAnalysis/CIStudies/interface/Analyzer.hh"
-
 #include "CIAnalysis/CIStudies/interface/DoubleMuonTrigger.hh"
 #include "CIAnalysis/CIStudies/interface/GenSimEventDumpModule.hh"
 #include "CIAnalysis/CIStudies/interface/GenSimIdentificationModule.hh"
+#include "CIAnalysis/CIStudies/interface/HistogramOutputModule.hh"
 #include "CIAnalysis/CIStudies/interface/LRWeightModule.hh"
 #include "CIAnalysis/CIStudies/interface/LeptonEfficiency.hh"
 #include "CIAnalysis/CIStudies/interface/MassRecoEfficiency.hh"
@@ -24,40 +25,41 @@
 Analyzer hPlusPlusMassAnalysis() {
   Analyzer analyzer;
 
-  auto eventDump = make_shared<GenSimEventDumpModule>(2);
+  auto eventDump = std::make_shared<GenSimEventDumpModule>(2);
 
-  auto genSimMod = make_shared<GenSimIdentificationModule>(9900041);
-  auto recoMod = make_shared<RecoIdentificationModule>(50);
-  auto matchMod = make_shared<MatchingModule>(genSimMod, recoMod);
-  auto triggerMod = make_shared<TriggerModule>(recoMod);
-  auto weightMod = make_shared<WeightingModule>();
-  auto lrWeightMod = make_shared<LRWeightModule>();
+  auto genSimMod = std::make_shared<GenSimIdentificationModule>(9900041);
+  auto recoMod = std::make_shared<RecoIdentificationModule>(50);
+  auto matchMod = std::make_shared<MatchingModule>(genSimMod, recoMod);
+  auto triggerMod = std::make_shared<TriggerModule>(recoMod);
+  auto weightMod = std::make_shared<WeightingModule>();
+  auto lrWeightMod = std::make_shared<LRWeightModule>();
 
-  auto nLeptonsFilter = make_shared<NLeptonsFilter>(
+  auto nLeptonsFilter = std::make_shared<NLeptonsFilter>(
       matchMod); // Needs to be updated with shared pointers
 
-  auto histMod = make_shared<HistogramOutputModule>(genSimMod, recoMod,
-                                                    weightMod, lrWeightMod);
+  auto histMod = std::make_shared<HistogramOutputModule>(
+      genSimMod, recoMod, weightMod, lrWeightMod);
   auto nLeptonsHist =
-      make_shared<NLeptonsHist>(matchMod, "Matched Leptons", 10, 0, 10);
-  auto nElectronsHist =
-      make_shared<NLeptonsHist>(matchMod, "Matched Electrons", 10, 0, 10, 11);
+      std::make_shared<NLeptonsHist>(matchMod, "Matched Leptons", 10, 0, 10);
+  auto nElectronsHist = std::make_shared<NLeptonsHist>(
+      matchMod, "Matched Electrons", 10, 0, 10, 11);
   auto nMuonsHist =
-      make_shared<NLeptonsHist>(matchMod, "Matched Muons", 10, 0, 10, 13);
+      std::make_shared<NLeptonsHist>(matchMod, "Matched Muons", 10, 0, 10, 13);
 
-  auto leptonEfficiency = make_shared<LeptonEfficiency>(matchMod, genSimMod);
+  auto leptonEfficiency =
+      std::make_shared<LeptonEfficiency>(matchMod, genSimMod);
   auto massRecoEfficiency200 =
-      make_shared<MassRecoEfficiency>(recoMod, 200, 10);
+      std::make_shared<MassRecoEfficiency>(recoMod, 200, 10);
   auto massRecoEfficiency500 =
-      make_shared<MassRecoEfficiency>(recoMod, 500, 25);
+      std::make_shared<MassRecoEfficiency>(recoMod, 500, 25);
   auto massRecoEfficiency800 =
-      make_shared<MassRecoEfficiency>(recoMod, 800, 40);
+      std::make_shared<MassRecoEfficiency>(recoMod, 800, 40);
   auto massRecoEfficiency1000 =
-      make_shared<MassRecoEfficiency>(recoMod, 1000, 50);
+      std::make_shared<MassRecoEfficiency>(recoMod, 1000, 50);
   auto massRecoEfficiency1300 =
-      make_shared<MassRecoEfficiency>(recoMod, 1300, 65);
+      std::make_shared<MassRecoEfficiency>(recoMod, 1300, 65);
 
-  auto recoThirdMuonPtHist = make_shared<ThirdMuonPtHist>(
+  auto recoThirdMuonPtHist = std::make_shared<ThirdMuonPtHist>(
       genSimMod, recoMod, false,
       std::string("Reconstructed Third Muon Transverse Momentum"), 50, 0, 3000);
 
@@ -65,12 +67,13 @@ Analyzer hPlusPlusMassAnalysis() {
   histMod->addHistogram(nLeptonsHist);
   histMod->addHistogram(nElectronsHist);
   histMod->addHistogram(nMuonsHist);
-  histMod->addHistogram(recoThirdMuonPtHist);
+  // histMod->addHistogram(recoThirdMuonPtHist);
 
   // Initialize triggers
-  auto singleMuonTrigger = make_shared<SingleMuonTrigger>(recoMod, 50);
-  auto doubleMuonTrigger = make_shared<DoubleMuonTrigger>(recoMod, 37, 27);
-  auto tripleMuonTrigger = make_shared<TripleMuonTrigger>(recoMod, 10, 5, 5);
+  auto singleMuonTrigger = std::make_shared<SingleMuonTrigger>(recoMod, 50);
+  auto doubleMuonTrigger = std::make_shared<DoubleMuonTrigger>(recoMod, 37, 27);
+  auto tripleMuonTrigger =
+      std::make_shared<TripleMuonTrigger>(recoMod, 10, 5, 5);
 
   // Add triggers to the TriggerModule
   triggerMod->addTrigger(singleMuonTrigger);
@@ -79,12 +82,12 @@ Analyzer hPlusPlusMassAnalysis() {
 
   analyzer.addProductionModule(genSimMod);
   analyzer.addProductionModule(recoMod);
-  // analyzer.addProductionModule(matchMod);
-  // analyzer.addProductionModule(triggerMod);
+  analyzer.addProductionModule(matchMod);
+  analyzer.addProductionModule(triggerMod);
 
   // Don't remove unless you don't want histograms
   analyzer.addAnalysisModule(histMod);
-  analyzer.addAnalysisModule(eventDump);
+  // analyzer.addAnalysisModule(eventDump);
   // analyzer.addAnalysisModule(leptonEfficiency);
   // analyzer.addAnalysisModule(massRecoEfficiency200);
   // analyzer.addAnalysisModule(massRecoEfficiency500);
--- CIStudies/src/HistogramOutputModule.cc
+++ CIStudies/src/HistogramOutputModule.cc
@@ -1,291 +1,295 @@
 #include "CIAnalysis/CIStudies/interface/HistogramOutputModule.hh"
 #include "CIAnalysis/CIStudies/interface/HistogramPrototype.hh"
 
+#include "CIAnalysis/CIStudies/interface/FileParams.hh"
 #include "CIAnalysis/CIStudies/interface/GenSimIdentificationModule.hh"
-#include "CIAnalysis/CIStudies/interface/RecoIdentificationModule.hh"
-#include "CIAnalysis/CIStudies/interface/WeightingModule.hh"
 #include "CIAnalysis/CIStudies/interface/LRWeightModule.hh"
 #include "CIAnalysis/CIStudies/interface/PtResolutionModule.hh"
-#include "CIAnalysis/CIStudies/interface/FileParams.hh"
+#include "CIAnalysis/CIStudies/interface/RecoIdentificationModule.hh"
+#include "CIAnalysis/CIStudies/interface/WeightingModule.hh"
 
 #include <iostream>
 #include <stdexcept>
 
 #include "TH1.h"
 
-HistogramOutputModule::HistogramOutputModule(const std::shared_ptr<GenSimIdentificationModule> genSimModule, const std::shared_ptr<RecoIdentificationModule> recoModule, const std::shared_ptr<WeightingModule> weightingModule, const std::shared_ptr<LRWeightModule> lrWeightModule) :
-  genSim(genSimModule),
-  reco(recoModule),
-  weighting(weightingModule),
-  lrWeighting(lrWeightModule)
-{
-}
+HistogramOutputModule::HistogramOutputModule(
+    const std::shared_ptr<GenSimIdentificationModule> genSimModule,
+    const std::shared_ptr<RecoIdentificationModule> recoModule,
+    const std::shared_ptr<WeightingModule> weightingModule,
+    const std::shared_ptr<LRWeightModule> lrWeightModule)
+    : genSim(genSimModule), reco(recoModule), weighting(weightingModule),
+      lrWeighting(lrWeightModule) {}
 
-void HistogramOutputModule::writeAll()
-{
+void HistogramOutputModule::writeAll() {
   // Check if any baseObjects have not been copied to objects yet,
   // and fill them into the main map if not
-  for (auto entry : baseObjects)
-    {
-      bool foundIt = false;
-      for (auto entry2 : objects)
-	{
-	  if (entry2.first.find(entry.first) != std::string::npos)
-	    {
-	      foundIt = true;
-	      break;
-	    }
-	}
+  for (auto entry : baseObjects) {
+    bool foundIt = false;
+    for (auto entry2 : objects) {
+      if (entry2.first.find(entry.first) != std::string::npos) {
+        foundIt = true;
+        break;
+      }
+    }
 
-      if (!foundIt)
-	{
-	  addObjectClone(entry.first, entry.first);
-	}
+    if (!foundIt) {
+      addObjectClone(entry.first, entry.first);
     }
-  
+  }
+
   // Then write all the objects to file
-  for (auto& entry : objects)
-    {
-      entry.second->Write();
-    }
+  for (auto &entry : objects) {
+    entry.second->Write();
+  }
 }
 
-void HistogramOutputModule::addObject(const std::string& name, TObject* obj)
-{
+void HistogramOutputModule::addObject(const std::string &name, TObject *obj) {
   if (baseObjects.find(name) == baseObjects.end())
     baseObjects.insert({name, obj});
   else
     baseObjects[name] = obj;
 
   // If this is called during finalize(), it needs to be added explicitly/
-  // The filter name is set properly during calls to finalize(), so 
+  // The filter name is set properly during calls to finalize(), so
   // this will work.
   if (isDone())
     addObjectClone(name, getFilter() + name);
 }
 
-void HistogramOutputModule::addMassBinObject(std::string name, std::string massbin)
-{
-  if (massBinMap.find(name) == massBinMap.end())  // 
+void HistogramOutputModule::addMassBinObject(std::string name,
+                                             std::string massbin) {
+  if (massBinMap.find(name) == massBinMap.end()) //
   {
     // std::cout << "Added Mass Bin: " << name << ", " << massbin << '\n';
-    std::vector<std::string> v;           // Create a vector
-    v.push_back(massbin);                 // Add the mass bin string to the vector
+    std::vector<std::string> v; // Create a vector
+    v.push_back(massbin);       // Add the mass bin string to the vector
     massBinMap.insert({name, v});
-  }
-  else
-  {
-    massBinMap[name].push_back(massbin);    // Add the mass bin string to the vector
+  } else {
+    massBinMap[name].push_back(
+        massbin); // Add the mass bin string to the vector
   }
 }
 
-void HistogramOutputModule::makeHistogram(const std::string& name, const std::string& title, int nbins, double min, double max)
-{
+void HistogramOutputModule::makeHistogram(const std::string &name,
+                                          const std::string &title, int nbins,
+                                          double min, double max) {
+  std::cout << "HISTOGRAM:: (min, max) -> (" << min << ", " << max << ")\n";
   auto newHist = new TH1F(name.c_str(), title.c_str(), nbins, min, max);
   addObject(name, newHist);
 }
 
-void HistogramOutputModule::makeHistogram(std::shared_ptr<HistogramPrototype> h)
-{
-  const std::string& name = h->getFilteredName();
+void HistogramOutputModule::makeHistogram(
+    std::shared_ptr<HistogramPrototype> h) {
+  const std::string &name = h->getFilteredName();
   // std::cout << "makeHistogram Name: " << name << '\n';
   auto title = name;
   int nbins = h->getNBins();
   double min = h->getMinimum();
   double max = h->getMaximum();
-  
+
+  std::cout << "HISTOGRAM:: (min, max) -> (" << min << ", " << max << ")\n";
   auto newHist = new TH1F(name.c_str(), title.c_str(), nbins, min, max);
   addObject(name, newHist);
 }
 
-void HistogramOutputModule::fillHistogram(const std::string& name, double number, double weight)
-{
+void HistogramOutputModule::fillHistogram(const std::string &name,
+                                          double number, double weight) {
+
   auto hist = getHistogram(name);
   if (!hist)
-    throw std::runtime_error("Argument to getHistogram was not of TH1 type!  Name: " + name 
-			     + " and Root type: " + getObject(name)->ClassName());
+    throw std::runtime_error(
+        "Argument to getHistogram was not of TH1 type!  Name: " + name +
+        " and Root type: " + getObject(name)->ClassName());
   hist->Fill(number, weight);
 }
 
-std::string HistogramOutputModule::getObjectName(const std::string& str) const
-{
+std::string HistogramOutputModule::getObjectName(const std::string &str) const {
   std::string newName = getFilter() + str;
   // std::cout << "Histogram Got: " << newName << '\n';
 
   // If the map has this already, great!
-  if (objects.find(newName) == objects.end()) 
-    {
-      // If not, see if the nonfiltered version exists
-      // and add a new member
-      if (baseObjects.find(str) != baseObjects.end())
-	{
-	  addObjectClone(str, newName);
-	}
-      // Otherwise, it must be an error
-      else
-	{
-	  throw std::out_of_range("Invalid key " + str + " passed to getObject()!");
-	}
+  if (objects.find(newName) == objects.end()) {
+    // If not, see if the nonfiltered version exists
+    // and add a new member
+    if (baseObjects.find(str) != baseObjects.end()) {
+      addObjectClone(str, newName);
     }
+    // Otherwise, it must be an error
+    else {
+      throw std::out_of_range("Invalid key " + str + " passed to getObject()!");
+    }
+  }
 
   return newName;
 }
 
-void HistogramOutputModule::addObjectClone(const std::string& oldName, 
-					   const std::string& newName) const
-{
+void HistogramOutputModule::addObjectClone(const std::string &oldName,
+                                           const std::string &newName) const {
   // Clone the object
-  TObject* clone = baseObjects.find(oldName)->second->Clone();
-  
+  TObject *clone = baseObjects.find(oldName)->second->Clone();
+
   // Rename it if possible
-  auto named = dynamic_cast<TNamed*>(clone);
-  if (named)
-     {
-        named->SetName(newName.c_str());
-     }
+  auto named = dynamic_cast<TNamed *>(clone);
+  if (named) {
+    named->SetName(newName.c_str());
+  }
 
   // Insert it into the map
   objects.insert({newName, clone});
 }
 
-bool HistogramOutputModule::process(const edm::EventBase& event)
-{
+bool HistogramOutputModule::process(const edm::EventBase &event) {
   // std::cout << "Process\n";
 
   std::string massBin = getFileParams().getMassRange();
   std::string helicity = getFileParams().getHelicity();
 
-  //std::cerr << "plot contrivance" << std::endl;
+  // std::cerr << "plot contrivance" << std::endl;
 
   double eventWeight = 1.00;
-  if (helicity == "LR")
-    {
-      eventWeight = lrWeighting->getLRWeight();
-    }
-  if (helicity == "RL")
-    {
-      eventWeight = lrWeighting->getRLWeight();
-    }
+  if (helicity == "LR") {
+    eventWeight = lrWeighting->getLRWeight();
+  }
+  if (helicity == "RL") {
+    eventWeight = lrWeighting->getRLWeight();
+  }
 
-  //std::cerr << "MS paint drawing" << std::endl;
+  // std::cerr << "MS paint drawing" << std::endl;
 
-  for (auto hist : histograms)
-  {
-    bool draw = hist->shouldDraw(event); // call the shouldDraw function so we can call process on the FilterModules
+  for (auto hist : histograms) {
+    bool draw =
+        hist->shouldDraw(event); // call the shouldDraw function so we can call
+                                 // process on the FilterModules
     // std::cout << "shouldDraw returns: " << draw << '\n';
-    
-    // If the mass bin is a new mass bin, then make the histograms for that mass bin
-    if (isNewMassBin(massBin))
-      {
-        auto weight = weighting->getWeight();
-        auto fileKey = getFileParams().getFileKey();
-    
-        massBins[massBin] = weight;
-        fileKeys[massBin] = fileKey;
 
-    //    // std::cout << "New Mass Bin: " << massBin << '\n';
-    //    // std::cout << "New Histogram Made: " << hist->getFilteredName() + massBin << '\n';
-    //    makeHistogram(hist->getFilteredName() + massBin, hist->getFilteredName() + massBin, hist->getNBins(), hist->getMinimum(), hist->getMaximum());
-    //    addMassBinObject(hist->getFilteredName(), massBin);
-        }
+    // If the mass bin is a new mass bin, then make the histograms for that mass
+    // bin
+    if (isNewMassBin(massBin)) {
+      auto weight = weighting->getWeight();
+      auto fileKey = getFileParams().getFileKey();
 
-    //std::cerr << "Shrek reference" << std::endl;
+      massBins[massBin] = weight;
+      fileKeys[massBin] = fileKey;
+
+      //    // std::cout << "New Mass Bin: " << massBin << '\n';
+      //    // std::cout << "New Histogram Made: " << hist->getFilteredName() +
+      //    massBin << '\n'; makeHistogram(hist->getFilteredName() + massBin,
+      //    hist->getFilteredName() + massBin, hist->getNBins(),
+      //    hist->getMinimum(), hist->getMaximum());
+      //    addMassBinObject(hist->getFilteredName(), massBin);
+    }
+
+    // std::cerr << "Shrek reference" << std::endl;
 
     // If the histogram with mass bin doesn't exist, make it
-    if (baseObjects.find(hist->getFilteredName() + massBin) == baseObjects.end())
-      {
-        // std::cout << "Name: " << hist->getName() << '\n';
-        // std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
-        // std::cout << "Mass Bin: " << massBin << '\n';
-        // std::cout << "Histogram missing and made (with Mass Bin): " << hist->getFilteredName() + massBin << '\n';
-        makeHistogram(hist->getFilteredName() + massBin, hist->getFilteredName() + massBin, hist->getNBins(), hist->getMinimum(), hist->getMaximum()); 
-        addMassBinObject(hist->getFilteredName(), massBin);
-      }
+    if (baseObjects.find(hist->getFilteredName() + massBin) ==
+        baseObjects.end()) {
+      std::cout << "HISTOGRAM:: making histgoram for massbin: " << massBin
+                << "\n";
+      // std::cout << "Name: " << hist->getName() << '\n';
+      // std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
+      // std::cout << "Mass Bin: " << massBin << '\n';
+      // std::cout << "Histogram missing and made (with Mass Bin): " <<
+      // hist->getFilteredName() + massBin << '\n';
+      makeHistogram(hist->getFilteredName() + massBin,
+                    hist->getFilteredName() + massBin, hist->getNBins(),
+                    hist->getMinimum(), hist->getMaximum());
+      addMassBinObject(hist->getFilteredName(), massBin);
+    }
 
-    //std::cerr << "2012 apocalypse" << std::endl;
+    // std::cerr << "2012 apocalypse" << std::endl;
 
     // If the histogram without mass bin doesn't exist, make it
-    if (baseObjects.find(hist->getFilteredName()) == baseObjects.end())
-      {
-        // std::cout << "Name: " << hist->getName() << '\n';
-        // std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
-        // std::cout << "Mass Bin: " << massBin << '\n';
-        // std::cout << "Histogram missing and made (without Mass Bin): " << hist->getFilteredName() << '\n';
-        makeHistogram(hist->getFilteredName(), hist->getFilteredName(), hist->getNBins(), hist->getMinimum(), hist->getMaximum()); 
-      }
+    if (baseObjects.find(hist->getFilteredName()) == baseObjects.end()) {
+      // std::cout << "Name: " << hist->getName() << '\n';
+      // std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
+      // std::cout << "Mass Bin: " << massBin << '\n';
+      // std::cout << "Histogram missing and made (without Mass Bin): " <<
+      // hist->getFilteredName() << '\n';
+      makeHistogram(hist->getFilteredName(), hist->getFilteredName(),
+                    hist->getNBins(), hist->getMinimum(), hist->getMaximum());
+    }
 
     // Fill the histogram if the filter string isn't empty
-    if (hist->getName() != hist->getFilteredName()) // If the filter string is empty, then the name and the filtered name should be the same
-      {
-         //std::cout << "Name: " << hist->getName() << '\n';
-         //std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
-         //std::cout << "Mass Bin: " << massBin << '\n';
-         //std::cout << "Histogram missing and made: " << hist->getFilteredName() << '\n';
-        makeHistogram(hist->getFilteredName(), hist->getFilteredName(), hist->getNBins(), hist->getMinimum(), hist->getMaximum()); 
-      }
+    if (hist->getName() !=
+        hist->getFilteredName()) // If the filter string is empty, then the name
+                                 // and the filtered name should be the same
+    {
+      // std::cout << "Name: " << hist->getName() << '\n';
+      // std::cout << "FilteredName: " << hist->getFilteredName() << '\n';
+      // std::cout << "Mass Bin: " << massBin << '\n';
+      // std::cout << "Histogram missing and made: " << hist->getFilteredName()
+      // << '\n';
+      makeHistogram(hist->getFilteredName(), hist->getFilteredName(),
+                    hist->getNBins(), hist->getMinimum(), hist->getMaximum());
+    }
 
-    //std::cerr << "value = " << hist->value() << std::endl;
+    // std::cerr << "value = " << hist->value() << std::endl;
 
     // Fill the histogram if shouldDraw(event) (draw) returns true
-    //std::cerr << "dunkin donuts" << std::endl;
-    if (draw)
-      {
-        // std::cout << "Histogram Filled: " << hist->getFilteredName() + massBin << '\n' << "Value: " << hist->value() << '\n';
-        fillHistogram(hist->getFilteredName() + massBin, hist->value(), eventWeight);
-      }
+    // std::cerr << "dunkin donuts" << std::endl;
+    if (draw) {
+      // std::cout << "\nHistogram Filled: " << hist->getFilteredName() +
+      // massBin
+      //           << '\n'
+      //           << "Value: " << hist->value() << '\n'
+      //           << "Event weight: " << eventWeight << '\n';
+
+      fillHistogram(hist->getFilteredName() + massBin, hist->value(),
+                    eventWeight);
+    }
   }
-  //std::cerr << "kumon: math, reading, success" << std::endl;
+  // std::cerr << "kumon: math, reading, success" << std::endl;
   return true;
 }
 
-void HistogramOutputModule::finalize()
-{
+void HistogramOutputModule::finalize() {
   // std::cout << "Finalize\n";
 
-  for (auto pair : massBinMap)
-  {
+  for (auto pair : massBinMap) {
     // std::cout << pair.first << '\n';
-    // std::cout << "Pair First: " << pair.first << "\t" << "Pair Second: " << pair.second << "\n";
+    // std::cout << "Pair First: " << pair.first << "\t" << "Pair Second: " <<
+    // pair.second << "\n";
 
-    for (auto massBin : massBins)
-      {
-        // std::cout << "Mass Bin: " << massBin.first << '\n';
-        auto fileKey = fileKeys[massBin.first];
-        auto eventCount = getEventCount(fileKey);
+    for (auto massBin : massBins) {
+      // std::cout << "Mass Bin: " << massBin.first << '\n';
+      auto fileKey = fileKeys[massBin.first];
+      auto eventCount = getEventCount(fileKey);
 
-        // std::cout << "Pair Second Size: " << pair.second.size() << "\n";
+      // std::cout << "Pair Second Size: " << pair.second.size() << "\n";
 
-        for (auto bin : pair.second)
-        {
-          // std::cout << "Bin: " << bin << "\t" << "Event Count: " << eventCount << "\n";
-          if (bin == massBin.first && eventCount != 0)
-          {
-            if (massBin.second != 0)
-            {
-              getHistogram(pair.first + bin)->Scale(massBin.second / eventCount);  // massBin.second is the scale
-            }
-
-            // for (int i = 1; i < getHistogram(pair.first)->GetNbinsX() ; ++i)
-            // {
-              // getHistogram(pair.first)->AddBinContent(i, getHistogram(pair.first + massBin.first)->GetBinContent(i));
-            // }
-            getHistogram(pair.first)->Add(getHistogram(pair.first + massBin.first));
+      for (auto bin : pair.second) {
+        // std::cout << "Bin: " << bin << "\t" << "Event Count: " << eventCount
+        // << "\n";
+        if (bin == massBin.first && eventCount != 0) {
+          std::cout << "HISTOGRAM:: scale: " << massBin.second / eventCount
+                    << "\n";
+          if (massBin.second != 0) {
+            getHistogram(pair.first + bin)->Scale(1);
+            // ->Scale(massBin.second /
+            //         eventCount); // massBin.second is the scale
           }
+
+          // for (int i = 1; i < getHistogram(pair.first)->GetNbinsX() ; ++i)
+          // {
+          // getHistogram(pair.first)->AddBinContent(i, getHistogram(pair.first
+          // + massBin.first)->GetBinContent(i));
+          // }
+          getHistogram(pair.first)
+              ->Add(getHistogram(pair.first + massBin.first));
         }
-      }    
+      }
+    }
   }
 }
 
-bool HistogramOutputModule::isNewMassBin(const std::string mass)
-{
-  for (auto massBin : massBins)
-    {
-      if (mass == massBin.first)
-	{
-	  return false;
-	}
+bool HistogramOutputModule::isNewMassBin(const std::string mass) {
+  for (auto massBin : massBins) {
+    if (mass == massBin.first) {
+      return false;
     }
-  
+  }
+
   return true;
 }
-
