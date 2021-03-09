0b63a9c07fe1b8378cae27faebf7b78eb8373953
Kodai Speich <kspeich@imsa.edu>
2021-03-09 10:32:20 -0600

    Updated TriggerEfficiencyModule

--- CIStudies/src/TriggerEfficiencyModule.cc
+++ CIStudies/src/TriggerEfficiencyModule.cc
@@ -1,6 +1,7 @@
 #include "CIAnalysis/CIStudies/interface/TriggerEfficiencyModule.hh"
 #include "CIAnalysis/CIStudies/interface/MatchingModule.hh"
 #include "CIAnalysis/CIStudies/interface/GenSimIdentificationModule.hh"
+#include "CIAnalysis/CIStudies/interface/Particle.hh"
 
 TriggerEfficiencyModule::TriggerEfficiencyModule(const std::shared_ptr<MatchingModule> iMatchMod, const std::shared_ptr<GenSimIdentificationModule> iGenSimMod, double iHiggsMass, double iWidth) :
   matchMod(iMatchMod),
@@ -12,18 +13,36 @@ TriggerEfficiencyModule::TriggerEfficiencyModule(const std::shared_ptr<MatchingM
 
 bool TriggerEfficiencyModule::process(const edm::EventBase& event)
 {
-  auto genSimParticles = genSimMod->getGenParticles().getParticles();
+  auto genSimParticles = genSimMod->getGenParticles().getParticles();  // Vector of Particles
 
-  for (auto genSimParticle1 : genSimParticles)
+  // std::cout << "Number of GenSim Particles: " << genSimParticles.size() << '\n';
+
+  for (size_t i = 0; i < genSimParticles.size(); ++i)
   {
-    for (auto genSimParticle2 : genSimParticles)
+    for (size_t j = i + 1; j < genSimParticles.size(); ++j)  // As to avoid overcounting
     {
+      auto genSimParticle1 = genSimParticles[i];
+      auto genSimParticle2 = genSimParticles[j];
+
+      //std::cout << "------------------------------------------------------------------------\n";
+      //std::cout << "GenSim Particle 1\n";
+      //printDebugLines(genSimParticle1);
+      //
+      //std::cout << "GenSim Particle 2\n";
+      //printDebugLines(genSimParticle2);
+      //
+      //std::cout << "Higgs Mass: " << higgsMass << '\n';
+
       if ((genSimParticle1 != genSimParticle2) && (genSimParticle1.uniqueMother() == genSimParticle2.uniqueMother()) && (genSimParticle1.charge() == genSimParticle2.charge()))
       {
         ParticleCollection genSimPair;
         genSimPair.addParticle(genSimParticle1);
         genSimPair.addParticle(genSimParticle2);
-                
+
+        // std::cout << "Particle Pair Invariant Mass: " << genSimPair.getInvariantMass() << "\n\n";
+        
+        ++expectedHiggsCount;
+
         if (((higgsMass - width) <= genSimPair.getInvariantMass()) && (genSimPair.getInvariantMass() <= (higgsMass + width)))
         {
           ++genSimCount;
@@ -58,7 +77,25 @@ bool TriggerEfficiencyModule::process(const edm::EventBase& event)
 
 void TriggerEfficiencyModule::finalize()
 {
+  std::cout << "Expected GenSim Higgs: " << expectedHiggsCount << '\n';
   std::cout << "Number of GenSim H++'s that fall into the mass window of the H++: " << genSimCount << '\n';
   std::cout << "Number of Reco H++'s that fall into the mass window of the H++: " << recoCount << '\n';
+  std::cout << "Overall Generator Eficiency for H++: " << static_cast<double>(genSimCount) / expectedHiggsCount << '\n';
   std::cout << "Overall Trigger Efficiency for H++: " << static_cast<double>(recoCount) / genSimCount << '\n';
 }
+
+void TriggerEfficiencyModule::printDebugLines(Particle particle) const  // So I know why the code is not working
+{
+  if (particle.getLeptonType() == Particle::LeptonType::Electron)
+  {
+    std::cout << "Type: Electron\n";
+  }
+  else if (particle.getLeptonType() == Particle::LeptonType::Muon)
+  {
+    std::cout << "Type: Muon\n";
+  }
+  std::cout << "PDGID: " << particle.pdgId() << '\n';
+  std::cout << "Charge: " << particle.charge() << '\n';
+  std::cout << "Mother PDGID: " << particle.uniqueMother().pdgId() << "\n\n";
+}
+  
