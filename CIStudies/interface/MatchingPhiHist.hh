a4842e77687a99f44d3560f139178b893738f71f
Sreyansh Mamidi <smamidi@imsa.edu>
2021-03-22 09:18:32 -0500

    Added new samples

---
+++ CIStudies/interface/MatchingPhiHist.hh
@@ -0,0 +1,19 @@
+#ifndef MATCHINGPHIHIST_HH
+#define MATCHINGPHIHIST_HH
+
+#include "HistogramPrototype.hh"
+
+class LeptonJetMatchingModule;
+
+class MatchingPhiHist : public HistogramPrototype
+{
+  public:
+  MatchingPhiHist(std::shared_ptr<LeptonJetMatchingModule> iLeptonJetMatchingModule, const std::string& iname, int iNBins, double iminimum, double imaximum);
+  double value() const override;
+
+  private:
+  std::shared_ptr<LeptonJetMatchingModule> leptonJetMatchingModule;
+
+};
+
+#endif
---
+++ CIStudies/src/MatchingPhiHist.cc
@@ -0,0 +1,33 @@
+#include "CIAnalysis/CIStudies/interface/MatchingPhiHist.hh"
+#include "CIAnalysis/CIStudies/interface/LeptonJetMatchingModule.hh"
+#include "CIAnalysis/CIStudies/interface/LeptonJet.hh"
+#include "DataFormats/Math/interface/normalizedPhi.h"
+
+#include <vector>
+
+MatchingPhiHist::MatchingPhiHist(std::shared_ptr<LeptonJetMatchingModule> iLeptonJetMatchingModule, const std::string& iname, int iNBins, double iminimum, double imaximum) :
+    HistogramPrototype(iname, iNBins, iminimum, imaximum),
+    leptonJetMatchingModule(iLeptonJetMatchingModule)
+
+{}
+
+double MatchingPhiHist::value() const
+{
+    const auto& matchingPairs = leptonJetMatchingModule->getMatchingPairs();
+    if (matchingPairs.size() != 0)
+    {
+        // std::cout << "MatchingPairs size not zero" << "\n";
+        auto pair = matchingPairs[0];
+        auto partPhi = pair.first.fourVector().Phi();
+        // std::cout << "MC phi value: " << partPhi << "\n";
+        auto jetPhi = pair.second.getFourVector().Phi();
+        // std::cout << "Reco phi value: " << jetPhi << "\n";
+        double deltaPhi = reco::deltaPhi(partPhi, jetPhi);
+
+        return deltaPhi;
+    }
+    else
+    {
+        return -1;
+    }
+}
