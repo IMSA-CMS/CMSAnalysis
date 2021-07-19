5e0909022a003df0bce643420c2fbc4d39c874d4
Kodai Speich <kspeich@imsa.edu>
2021-07-18 23:51:33 -0500

    Modified Same-Sign Invariant Mass methods to return values by separating positive and negative particles

--- CIStudies/src/SameSignInvariantMassHist.cc
+++ CIStudies/src/SameSignInvariantMassHist.cc
@@ -4,19 +4,43 @@
 #include "CIAnalysis/CIStudies/interface/GenSimIdentificationModule.hh"
 #include "CIAnalysis/CIStudies/interface/RecoIdentificationModule.hh"
 
+SameSignInvariantMassHist::SameSignInvariantMassHist(const std::shared_ptr<GenSimIdentificationModule> genSimModule, const std::shared_ptr<RecoIdentificationModule> recoModule, const bool typeSwitch, const std::string& iname, int iNBins, double iminimum, double imaximum, bool iUsingPhi, bool iMultipleMasses) :
+  GenSimRecoPrototype(genSimModule, recoModule, typeSwitch, iname, iNBins, iminimum, imaximum),
+  usingPhi(iUsingPhi),
+  multipleMasses(iMultipleMasses)
+{
+}
+
 std::vector<double> SameSignInvariantMassHist::protectedValue(bool typeGenSim) const
 {
+  if (multipleMasses)
+  {
+    if (typeGenSim)        // typeGenSim == true, so we want the GenSim values
+    {
+      auto genParticles = getGenSim()->getGenParticles();
+      auto genSimInv = genParticles.calculateSameSignInvariantMasses(usingPhi);
+      return {genSimInv};
+    }
+
+    else                   // typeGenSim == false, so we want the Reco values
+    {
+      auto recoParticles = getReco()->getRecoCandidates();
+      auto recoInv = recoParticles.calculateSameSignInvariantMasses(usingPhi);
+      return {recoInv};
+    }
+  }
+
   if (typeGenSim)          // typeGenSim == true, so we want the GenSim values
   {
     auto genParticles = getGenSim()->getGenParticles();
-    auto genSimInv = genParticles.calculateSameSignInvariantMass();
+    auto genSimInv = genParticles.calculateSameSignInvariantMass(usingPhi);
     return {genSimInv};
   }
 
   else                     // typeGenSim == false, so we want the Reco values
   {
     auto recoParticles = getReco()->getRecoCandidates();
-    auto recoInv = recoParticles.calculateSameSignInvariantMass();
+    auto recoInv = recoParticles.calculateSameSignInvariantMass(usingPhi);
     return {recoInv};
   }
 }
