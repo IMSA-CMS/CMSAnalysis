88221a6beac7f635675f87c26e09a1e88936c558
Andy Tang <winterwind2022@gmail.com>
2021-10-21 16:01:00 -0500

    some files

--- CIStudies/bin/runAnalyzer.cc
+++ CIStudies/bin/runAnalyzer.cc
@@ -9,11 +9,10 @@
 #include "CIAnalysis/CIStudies/bin/massResolutionAnalysis.cc"
 #include "CIAnalysis/CIStudies/bin/HPlusPlusMassAnalysis.cc"
 // Include copy that isn't going to be changed -- FIX once work is done [09/17]
-#include "CIAnalysis/CIStudies/bin/BackgroundEstimateAnalysis.cc"
+#include "CIAnalysis/CIStudies/bin/LeptonJetBackgroundAnalysis.cc"
 #include "CIAnalysis/CIStudies/bin/leptonJetReconstructionAnalysis.cc"
 #include "CIAnalysis/CIStudies/bin/displacedVertexAnalysis.cc"
 #include "CIAnalysis/CIStudies/bin/massAcceptanceAnalysis.cc"
-#include "CIAnalysis/CIStudies/bin/triggerAnalysis.cc"
 
 
 int main(int argc, char **argv) {
@@ -48,7 +47,7 @@ int main(int argc, char **argv) {
 
   // USING ANALYER FOR H++ THAT WON'T CHANGE: CHANGE BACK
   // TO hPlusPlusMassAnalysis() when ready -- [09/17]
-  Analyzer analyzer = backgroundEstimateAnalysis();
+  Analyzer analyzer = leptonJetBackgroundAnalysis();
   // Analyzer analyzer = massResolutionAnalysis();
   // Analyzer analyzer = leptonJetReconstructionAnalysis();
   // Analyzer analyzer = displacedVertexAnalysis();
