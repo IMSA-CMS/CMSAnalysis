914b7e288085691b95f325c5f4fbf07b96144f5c
Andy Tang <winterwind2022@gmail.com>
2021-11-03 11:02:54 -0500

    trees conduct photosynthesis, unless it's physics

--- CIStudies/bin/LeptonJetBackgroundAnalysis.cc
+++ CIStudies/bin/LeptonJetBackgroundAnalysis.cc
@@ -5,12 +5,10 @@
 #include "TSystem.h"
 
 #include "CIAnalysis/CIStudies/interface/Analyzer.hh"
-#include "CIAnalysis/CIStudies/interface/GenSimIdentificationModule.hh"
 #include "CIAnalysis/CIStudies/interface/HistogramOutputModule.hh"
 #include "CIAnalysis/CIStudies/interface/LRWeightModule.hh"
 #include "CIAnalysis/CIStudies/interface/NLeptonJetHist.hh"
 #include "CIAnalysis/CIStudies/interface/LeptonJetReconstructionModule.hh"
-#include "CIAnalysis/CIStudies/interface/RecoIdentificationModule.hh"
 #include "CIAnalysis/CIStudies/interface/WeightingModule.hh"
 
 
@@ -19,21 +17,21 @@ using std::make_shared;
 Analyzer leptonJetBackgroundAnalysis() {
   Analyzer analyzer;
 
-  auto genSimMod = make_shared<GenSimIdentificationModule>(1000022, true);
-  auto recoMod = make_shared<RecoIdentificationModule>(5);
+  // auto genSimMod = make_shared<GenSimIdentificationModule>(1000022, true);
+  // auto recoMod = make_shared<RecoIdentificationModule>(5);
   auto weightMod = make_shared<WeightingModule>();
   auto lrWeightMod = make_shared<LRWeightModule>();
   
-  auto leptonJetRecoModule = make_shared<LeptonJetReconstructionModule>(recoMod, 0.05);
+  auto leptonJetRecoModule = make_shared<LeptonJetReconstructionModule>(0.05);
   
   auto nLeptonJetsHist = make_shared<NLeptonJetHist>(leptonJetRecoModule, "Number of Lepton Jets", 10, 0, 10);
-  auto histMod = make_shared<HistogramOutputModule>(genSimMod, recoMod, weightMod, lrWeightMod);
+  auto histMod = make_shared<HistogramOutputModule>(weightMod, lrWeightMod);
 
 
   histMod->addHistogram(nLeptonJetsHist);
 
-  analyzer.addProductionModule(genSimMod);
-  analyzer.addProductionModule(recoMod);
+  // analyzer.addProductionModule(genSimMod);
+  // analyzer.addProductionModule(recoMod);
   analyzer.addProductionModule(leptonJetRecoModule);
   analyzer.addProductionModule(weightMod);
   analyzer.addProductionModule(lrWeightMod);
