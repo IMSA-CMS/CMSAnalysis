044d915a3339fce5e9c994803b9e3655ceb03015
Eric Shackelford <eshackelford@imsa.edu>
2021-11-08 14:12:02 -0600

    Automating Event File recognition

--- CIStudies/interface/EventLoader.hh
+++ CIStudies/interface/EventLoader.hh
@@ -2,10 +2,12 @@
 #define EVENTLOADER_HH
 
 #include "ParticleCollection.hh"
+#include "EventFile.hh"
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
 #include "FWCore/Common/interface/TriggerNames.h"
+
 
 
 class TFile;
@@ -13,32 +15,23 @@ class TFile;
 class EventLoader
 {
     public:
-        EventLoader(int outputEvery) : numOfEvents(0), eventCount(0), outputEvery(outputEvery) {}
-        virtual ~EventLoader() {}
-        void changeFile(TFile* jfile) {file = jfile; newFile(jfile);}
-        virtual void nextEvent() = 0;
-        virtual bool isDone() const = 0;
+        EventLoader(int outputEvery) : outputEvery(outputEvery) {}
+        void changeFile(TFile* ifile);
+        std::shared_ptr<EventFile> getFile() {return file;}
         // simply dumps gensim particles from event into ParticleCollection
-        virtual ParticleCollection getGenSimParticles() const = 0;
-        virtual ParticleCollection getRecoParticles() const = 0;
         //virtual std::vector<PileupSummaryInfo> getPileupInfo() const = 0;
-        virtual GenEventInfoProduct getGenInfo() const = 0;
+        
         // virtual edm::TriggerResults getTriggerResults(std::string subProcess) const = 0;
         // virtual edm::TriggerNames getTriggerNames(std::string subProcess) const = 0;
-        virtual double getMET() const = 0;
+        
     protected:
-        TFile* getFile() {return file;}
-        const TFile* getFile() const {return file;}
-        virtual void newFile(TFile* ifile) = 0;
-        int getNumOfEvents() const {return numOfEvents;}
-        void setNumOfEvents(int newNum) {numOfEvents = newNum;}
-        int getEventCount() const {return eventCount;}
-        void setEventCount(int newNum) {eventCount = newNum;}
+        //TFile* getFile() {return file;}
+        //const TFile* getFile() const {return file;}
+        //virtual void newFile(TFile* ifile) = 0;
     private:
-        TFile* file;
-        int numOfEvents;
-        int eventCount;
+        //TFile* file;
         int outputEvery;
+        std::shared_ptr<EventFile> file;
 };
 
 
