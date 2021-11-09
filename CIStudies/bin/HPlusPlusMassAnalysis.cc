819907a5cf6d2b673fb9c728ddc4a7b4beef4d88
Nathaniel Graf <ndgraf@cmslpc150.fnal.gov>
2021-11-09 11:36:10 -0600

    BackgroundEstCode

--- CIStudies/bin/HPlusPlusMassAnalysis.cc
+++ CIStudies/bin/HPlusPlusMassAnalysis.cc
@@ -48,66 +48,69 @@ Analyzer hPlusPlusMassAnalysis() {
   auto unusualFinalStateFilter = make_shared<UnusualFinalStateFilter>(recoMod);
   
   auto histMod = make_shared<HistogramOutputModule>(genSimMod, recoMod, weightMod, lrWeightMod);
-  auto nLeptonsHist = make_shared<NLeptonsHist>(matchMod, "Matched Leptons", 10, 0, 10);
-  auto nElectronsHist = make_shared<NLeptonsHist>(matchMod, "Matched Electrons", 10, 0, 10, 11);
-  auto nMuonsHist = make_shared<NLeptonsHist>(matchMod, "Matched Muons", 10, 0, 10, 13);
+  // auto nLeptonsHist = make_shared<NLeptonsHist>(matchMod, "Matched Leptons", 10, 0, 10);
+  // auto nElectronsHist = make_shared<NLeptonsHist>(matchMod, "Matched Electrons", 10, 0, 10, 11);
+  // auto nMuonsHist = make_shared<NLeptonsHist>(matchMod, "Matched Muons", 10, 0, 10, 13);
 
-  auto leptonEfficiency = make_shared<LeptonEfficiency>(matchMod, genSimMod);
+  // auto leptonEfficiency = make_shared<LeptonEfficiency>(matchMod, genSimMod);
 
-  auto massRecoEfficiency55 = make_shared<MassRecoEfficiency>(recoMod, 800, 5, 5);
-  auto massRecoEfficiency1010 = make_shared<MassRecoEfficiency>(recoMod, 800, 10, 10);
-  auto massRecoEfficiency4010 = make_shared<MassRecoEfficiency>(recoMod, 800, 40, 10);
-  auto massRecoEfficiency4040 = make_shared<MassRecoEfficiency>(recoMod, 800, 40, 40);
-  auto massRecoEfficiency8040 = make_shared<MassRecoEfficiency>(recoMod, 800, 80, 40);
-  auto massRecoEfficiency20080 = make_shared<MassRecoEfficiency>(recoMod, 800, 200, 80);
+  auto invariantMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, true, "Invariant Mass", 100, 0, 1000);
+  histMod->addHistogram(invariantMassHist);
 
-  auto triggerEfficiencyMod4010 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 40, 10);
-  auto triggerEfficiencyMod4040 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 40, 40);
-  auto triggerEfficiencyMod8040 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 80, 40);
-  auto triggerEfficiencyMod20080 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 200, 80);
+  // auto massRecoEfficiency55 = make_shared<MassRecoEfficiency>(recoMod, 800, 5, 5);
+  // auto massRecoEfficiency1010 = make_shared<MassRecoEfficiency>(recoMod, 800, 10, 10);
+  // auto massRecoEfficiency4010 = make_shared<MassRecoEfficiency>(recoMod, 800, 40, 10);
+  // auto massRecoEfficiency4040 = make_shared<MassRecoEfficiency>(recoMod, 800, 40, 40);
+  // auto massRecoEfficiency8040 = make_shared<MassRecoEfficiency>(recoMod, 800, 80, 40);
+  // auto massRecoEfficiency20080 = make_shared<MassRecoEfficiency>(recoMod, 800, 200, 80);
 
-  auto recoThirdMuonPtHist = make_shared<ThirdMuonPtHist>(genSimMod, recoMod, false, std::string("Reconstructed Third Muon Transverse Momentum"), 50, 0, 3000);
-  auto genSimSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, true, "GenSim Same Sign Invariant Mass", 100, 0, 1000);
-  auto recoSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Mass", 100, 0, 1000);
+  // auto triggerEfficiencyMod4010 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 40, 10);
+  // auto triggerEfficiencyMod4040 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 40, 40);
+  // auto triggerEfficiencyMod8040 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 80, 40);
+  // auto triggerEfficiencyMod20080 = make_shared<TriggerEfficiencyModule>(matchMod, genSimMod, 800, 200, 80);
 
-  auto recoPhiSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Mass (by Phi)", 100, 0, 1000, true);
-  auto recoMultSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Masses", 100, 0, 1000, false, true);
-  auto recoPhiMultSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Masses (by Phi)", 100, 0, 1000, true, true);
+  // auto recoThirdMuonPtHist = make_shared<ThirdMuonPtHist>(genSimMod, recoMod, false, std::string("Reconstructed Third Muon Transverse Momentum"), 50, 0, 3000);
+  // auto genSimSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, true, "GenSim Same Sign Invariant Mass", 100, 0, 1000);
+  // auto recoSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Mass", 100, 0, 1000);
 
-  auto genSimHPlusPlusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, true, "GenSim H++ Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
-  auto recoHPlusPlusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, false, "Reco H++ Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
-  auto genSimHMinusMinusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, true, "GenSim H-- Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, -9900041);
-  auto recoHMinusMinusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, false, "Reco H-- Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
+  // auto recoPhiSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Mass (by Phi)", 100, 0, 1000, true);
+  // auto recoMultSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Masses", 100, 0, 1000, false, true);
+  // auto recoPhiMultSameSignInvMassHist = make_shared<SameSignInvariantMassHist>(genSimMod, recoMod, false, "Reco Same Sign Invariant Masses (by Phi)", 100, 0, 1000, true, true);
 
-  auto photonHist = make_shared<PhotonsHist>(genSimMod, recoMod, true, "Photon Histogram", 100, 0, 1000);
+  // auto genSimHPlusPlusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, true, "GenSim H++ Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
+  // auto recoHPlusPlusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, false, "Reco H++ Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
+  // auto genSimHMinusMinusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, true, "GenSim H-- Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, -9900041);
+  // auto recoHMinusMinusRecoveredInvMassHist = make_shared<RecoveredInvariantMassHist>(genSimMod, recoMod, false, "Reco H-- Recovered Invariant Mass with 3 Leptons", 100, 0, 1000, 3, 9900041);
 
-  auto positiveNegativeInvMassHist = make_shared<TwoInvariantMassesHist>("Reco ZZ Invariant Mass Background", 100, 100, 0, 0, 1000, 1000, recoMod);
+  // auto photonHist = make_shared<PhotonsHist>(genSimMod, recoMod, true, "Photon Histogram", 100, 0, 1000);
 
-  // Add the histogram(s) created above to histMod
-  //histMod->addHistogram(recoThirdMuonPtHist);
-  //histMod->addHistogram(recoSameSignInvMassHist);
-  //histMod->addHistogram(genSimHPlusPlusRecoveredInvMassHist);
-  //histMod->addHistogram(recoHPlusPlusRecoveredInvMassHist);
-  //histMod->addHistogram(genSimHMinusMinusRecoveredInvMassHist);
-  //histMod->addHistogram(recoHMinusMinusRecoveredInvMassHist);
-  //histMod->addHistogram(photonHist);
-  //histMod->addHistogram(nLeptonsHist);
-  //histMod->addHistogram(nElectronsHist);
-  //histMod->addHistogram(nMuonsHist);
-  //histMod->addHistogram(recoPhiSameSignInvMassHist);
-  //histMod->addHistogram(recoMultSameSignInvMassHist);
-  //histMod->addHistogram(recoPhiMultSameSignInvMassHist);
-  histMod->addHistogram(positiveNegativeInvMassHist);
+  // auto positiveNegativeInvMassHist = make_shared<TwoInvariantMassesHist>("Reco ZZ Invariant Mass Background", 100, 100, 0, 0, 1000, 1000, recoMod);
 
-  // Initialize triggers
-  auto singleMuonTrigger = make_shared<SingleMuonTrigger>(recoMod, 50);
-  auto doubleMuonTrigger = make_shared<DoubleMuonTrigger>(recoMod, 37, 27);
-  auto tripleMuonTrigger = make_shared<TripleMuonTrigger>(recoMod, 10, 5, 5);
+  // // Add the histogram(s) created above to histMod
+  // //histMod->addHistogram(recoThirdMuonPtHist);
+  // //histMod->addHistogram(recoSameSignInvMassHist);
+  // //histMod->addHistogram(genSimHPlusPlusRecoveredInvMassHist);
+  // //histMod->addHistogram(recoHPlusPlusRecoveredInvMassHist);
+  // //histMod->addHistogram(genSimHMinusMinusRecoveredInvMassHist);
+  // //histMod->addHistogram(recoHMinusMinusRecoveredInvMassHist);
+  // //histMod->addHistogram(photonHist);
+  // //histMod->addHistogram(nLeptonsHist);
+  // //histMod->addHistogram(nElectronsHist);
+  // //histMod->addHistogram(nMuonsHist);
+  // //histMod->addHistogram(recoPhiSameSignInvMassHist);
+  // //histMod->addHistogram(recoMultSameSignInvMassHist);
+  // //histMod->addHistogram(recoPhiMultSameSignInvMassHist);
+  // histMod->addHistogram(positiveNegativeInvMassHist);
 
-  // Add triggers to the TriggerModule
-  triggerMod->addTrigger(singleMuonTrigger);
-  triggerMod->addTrigger(doubleMuonTrigger);
-  triggerMod->addTrigger(tripleMuonTrigger);
+  // // Initialize triggers
+  // auto singleMuonTrigger = make_shared<SingleMuonTrigger>(recoMod, 50);
+  // auto doubleMuonTrigger = make_shared<DoubleMuonTrigger>(recoMod, 37, 27);
+  // auto tripleMuonTrigger = make_shared<TripleMuonTrigger>(recoMod, 10, 5, 5);
+
+  // // Add triggers to the TriggerModule
+  // triggerMod->addTrigger(singleMuonTrigger);
+  // triggerMod->addTrigger(doubleMuonTrigger);
+  // triggerMod->addTrigger(tripleMuonTrigger);
 
   analyzer.addProductionModule(genSimMod);
   analyzer.addProductionModule(recoMod);
@@ -117,7 +120,7 @@ Analyzer hPlusPlusMassAnalysis() {
   analyzer.addProductionModule(lrWeightMod);
 
   // Filters
-  //analyzer.addFilterModule(nLeptonsFilter);
+  analyzer.addFilterModule(nLeptonsFilter);
   //analyzer.addFilterModule(unusualFinalStateFilter);
 
   analyzer.addAnalysisModule(histMod); // Don't remove unless you don't want histograms
--- CIStudies/bin/InvariantMassFitScript.C
+++ CIStudies/bin/InvariantMassFitScript.C
@@ -5,90 +5,324 @@
 #include <TFile.h>
 #include <TH1F.h>
 #include <TAxis.h>
+#include <sstream>
+#include <fstream>
+#include <array>
+#include "cstdio"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "../interface/TDisplayText.h"
+//swap out_file to cout if you want to print to terminal. 
+#define outout out_file
 
-void InvariantMassFitScript()
-{    
+struct Parameters
+    {
+        
+        double massTarget;
+        std::string fileName;
+        double totalEvents;
+        std::string histograms;
+        double crossSection;
+        double signalSwitch;
+        std::string analysisName;
+        std::string nickname;
+        double sumSwitch;
+
+
+        Parameters(std::string line)
+        {   
+            std::string eventsString, massTargetString, crossSectionString, signalSwitchString, sumSwitchString; 
+            std::istringstream str(line);
+            std::getline(str, massTargetString, '\t');
+            std::getline(str, fileName, '\t'); 
+            std::getline(str, eventsString, '\t');
+            std::getline(str, histograms, '\t');
+            std::getline(str, crossSectionString, '\t');
+            std::getline(str, signalSwitchString, '\t');
+            std::getline(str, analysisName, '\t');
+            std::getline(str, nickname, '\t');
+            std::getline(str, sumSwitch, '\t');
+            
+            massTarget = std::stod(massTargetString);
+            totalEvents = std::stod(eventsString);
+            crossSection = std::stod(crossSectionString);
+            signalSwitch = std::stod(signalSwitchString);
+            sumSwitch = std::stod(sumSwitchString);
+            //Change this from 4 to 3 depending on which lepton bin you want to analyze
+            analysisName = "4" + analysisName;
+
+        }
+    };
+
+void ComputationalFunction(Parameters param, std::ostream& out_file)
+    {
+
+        //Opens my file, assigns it f
+        TFile *f = new TFile(param.fileName.c_str()); //Change this for different files
+        // outout << param.fileName;
+        outout << param.nickname;
+        outout << " ";
+        TDisplayText *totalevents = f->Get<TDisplayText>("NEvents");
+        
+
+        //Takes the histogram wanted from the file, assigns it hist
+        TH1 *hist = dynamic_cast<TH1*>(f->Get(param.histograms.c_str())); //Change this for different fit 
+        if (!hist)
+            throw std::runtime_error ("Hist not found");
+
+        //Creates a power law fit function
+        TF1 *fitfunc = new TF1("fitfunc", "[0]*pow(x, [1])", 200, 1300);
+
+        //Fits the histogram with the fit function
+        hist->Fit(fitfunc, "Q0", "", 200, 1300);
+
+        //Finds the number of events in the analyzed hist
+        TH1F *histanalysis = (TH1F*)f->Get(param.analysisName.c_str()); //Change this for different hist analysis
+        double eventsanalysishist = histanalysis->GetEntries();
+        // outout << "\neventsanalysishist \n";
+        // outout << eventsanalysishist;
+
+        //Finds number of events ran total (from spreadsheet)
+        double totaleventsran = param.totalEvents; //Change this for different event numbers
+        // outout << "\ntotaleventsran \n";
+        // outout << totaleventsran;
+
+        //Finds lepton hist fraction
+        double efficiency = eventsanalysishist/totaleventsran;
+        // outout << "\nefficiency \n";
+        // outout << efficiency;
+
+        //Finds the integral of the fit from the mass target range +-5%
+        double acceptedCenter = param.massTarget;
+    
+        
+        double masslowaccepted = acceptedCenter - (acceptedCenter * .05);
+        double masshighaccepted = acceptedCenter + (acceptedCenter * .05);
+        double acceptedfitintegral = fitfunc->Integral(masslowaccepted, masshighaccepted);
+        // outout << "\nacceptedfitintegral \n";
+        // outout << acceptedfitintegral;
+        
+        //Finds the integral of the fit from 150-1300
+        double rangefitintegral = fitfunc->Integral(150, 1300);
+        // outout << "\nrangefitintegral \n";
+        // outout << rangefitintegral;
+
+        //Finds the integral of the analysis histogram from 0-1500
+        double totalanalysishistintegral = histanalysis->Integral();
+        // outout << "\ntotalanalysishistintegral \n";
+        // outout << totalanalysishistintegral;
+
+        //Finds the # of 4L events in the fit range
+        double analysis150bin = histanalysis->FindBin(150);
+        double analysis1300bin = histanalysis->FindBin(1300);
+        double eventanalysisfitrange = (histanalysis->Integral(analysis150bin, analysis1300bin)/totalanalysishistintegral);
+        // outout << "\neventanalysisfitrange \n";
+        // outout << eventanalysisfitrange;
+
+        //Calculates ratio between both fit integrals
+        double fitratio = acceptedfitintegral/rangefitintegral;
+        // outout << "\nfitratio \n";
+        // outout << fitratio;
+
+        //Finds the integral of the fit histogram from 0-1500
+        double totalhistintegral = hist->Integral(0, 1500);
+        // outout << "\ntotalhistintegral \n";
+        // outout << totalhistintegral;
+
+        // //Finds the integral of the fit histogram from 150-1300
+        // double bin150 = hist->FindBin(150);
+        // double bin1300 = hist->FindBin(1300);
+        // double rangehistintegral = hist->Integral(bin150, bin1300);
+        // outout << "\nrangehistintegral \n";
+        // outout << rangehistintegral;
+
+        //Finds histogram fraction
+        double histfraction = fitratio*(eventanalysisfitrange);
+        // outout << "\nhistfraction \n";
+        // outout << histfraction;
+
+        //Finds luminosity (from spreadsheet)
+        double luminosity = 137;
+        // outout << "\nluminosity \n";
+        // outout << luminosity;
+
+        //Finds crosssection (from spreadsheet)
+        double crosssection = param.crossSection; //Change this for different cross section
+        // outout << "\ncrosssection \n";
+        // outout << crosssection;     
+
+
+        //Finds backgroundest
+        double backgroundest = efficiency*crosssection*1000*luminosity*histfraction;
+        outout << backgroundest;
+        outout << "\n";
+
+        if (param.sumSwitch == 1)
+            oneSum = (backgroundest + oneSum);
+        if (param.sumSwitch == 2)
+            twoSum = (backgroundest + twoSum);
+
+
+        //f->Close();
+        delete histanalysis;
+        delete fitfunc;
+        delete hist;
+        delete f;
+
+        
+    }
+
+void SignalFunction(Parameters param, std::ostream& out_file)
+{
     //Opens my file, assigns it f
-    TFile *f = new TFile("DYMM50Sorted.root");
+    TFile *f = new TFile(param.fileName.c_str()); //Change this for different files
+    //outout << param.fileName;
+    //outout << "\n";
 
     //Takes the histogram wanted from the file, assigns it hist
-    TH1 *hist = dynamic_cast<TH1*>(f->Get("2Same Sign Invariant Mass"));
+    TH1 *hist = dynamic_cast<TH1*>(f->Get(param.histograms.c_str())); //Change this for different fit 
     if (!hist)
         throw std::runtime_error ("Hist not found");
 
-    //Creates a canvas
-    TCanvas *canvas = new TCanvas("canvas", 0, 1500);
-    canvas->SetLogy();
-
-    //Creates an exponential fit function
-    TF1 *fitfunc = new TF1("fitfunc", "[0]*pow(x, [1])", 200, 1300);
-
-    //Draws the hist
-    hist->Draw();
-
-    //Fits the histogram with the fit function
-    hist->Fit(fitfunc, "", "", 200, 1300);
-
-    //Finds the number of events in the 4 lepton hist
-    TH1F *hist4l = (TH1F*)f->Get("4Same Sign Invariant Mass");
-    double events4lhist = hist4l->GetEntries();
-    cout << "\nevents4lhist \n";
-    cout << events4lhist;
+    //Finds the number of events in the analyzed hist
+    TH1F *histanalysis = (TH1F*)f->Get(param.analysisName.c_str()); //Change this for different hist analysis
+    double eventsanalysishist = histanalysis->GetEntries();
+    // outout << "\neventsanalysishist \n";
+    // outout << eventsanalysishist;
 
     //Finds number of events ran total (from spreadsheet)
-    double totaleventsran = 2982000;
-    cout << "\ntotaleventsran \n";
-    cout << totaleventsran;
+    double totaleventsran = param.totalEvents; //Change this for different event numbers
+    // outout << "\ntotaleventsran \n";
+    // outout << totaleventsran;
 
-    //Finds 4 lepton fraction
-    double fraction4l = events4lhist/totaleventsran;
-    cout << "\nfraction4l \n";
-    cout << fraction4l;
+    //Finds lepton hist fraction
+    double efficiency = eventsanalysishist/totaleventsran;
+    // outout << "\nefficiency \n";
+    // outout << efficiency;
 
-    //Finds the integral of the fit from 760-840
-    double acceptedfitintegral = fitfunc->Integral(760, 840);
-    cout << "\nacceptedfitintegral \n";
-    cout << acceptedfitintegral;
-    
-    //Finds the integral of the fit from 150-1300
-    double rangefitintegral = fitfunc->Integral(150, 1300);
-    cout << "\nrangefitintegral \n";
-    cout << rangefitintegral;
-
-    //Calculates ratio between both fit integrals
-    double fitratio = acceptedfitintegral/rangefitintegral;
-    cout << "\nfitratio \n";
-    cout << fitratio;
-
-    //Finds the integral of the histogram from 0-1500
-    double totalhistintegral = hist->Integral(0, 1500);
-    cout << "\ntotalhistintegral \n";
-    cout << totalhistintegral;
-
-    //Finds the integral of the histogram from 150-1300
-    double bin150 = hist->FindBin(150);
-    double bin1300 = hist->FindBin(1300);
-    double rangehistintegral = hist->Integral(bin150, bin1300);
-    cout << "\nrangehistintegral \n";
-    cout << rangehistintegral;
-
-    //Finds histogram fraction
-    double histfraction = fitratio*(rangehistintegral/totalhistintegral);
-    cout << "\nhistfraction \n";
-    cout << histfraction;
+    //Finds the integral of the analysis histogram from 0-1500
+    double totalanalysishistintegral = histanalysis->Integral();
 
     //Finds luminosity (from spreadsheet)
-    double luminosity = 137;
-    cout << "\nluminosity \n";
-    cout << luminosity;
+    double luminosity = 3000;
+    // outout << "\nluminosity \n";
+    // outout << luminosity;
+
+    //Finds the integral of the fit from the mass target range +-5%
+    double acceptedCenter = param.massTarget; 
+    double masslowaccepted = acceptedCenter - (acceptedCenter * .05);
+    double masshighaccepted = acceptedCenter + (acceptedCenter * .05);
+   
 
     //Finds crosssection (from spreadsheet)
-    double crosssection = 2113;
-    cout << "\ncrosssection \n";
-    cout << crosssection;
+    double crosssection = param.crossSection; //Change this for different cross section
+    // outout << "\ncrosssection \n";
+    // outout << crosssection;
 
-    //Finds signal
-    double signal = fraction4l*crosssection*1000*luminosity*histfraction;
-    cout << "\nsignal \n";
-    cout << signal;
+    //Finds the # of 4L events in the fit range
+    double analysislowboundbin = histanalysis->FindBin(masslowaccepted);
+    double analysisupboundbin = histanalysis->FindBin(masshighaccepted);
+    double fraction4l = (histanalysis->Integral(analysislowboundbin, analysisupboundbin)/totalanalysishistintegral);
+    // outout << "hist integral thing";
+    // outout << "\n";
+    // outout << histanalysis->Integral(analysislowboundbin, analysisupboundbin);
+    // outout << "\n";
+    // outout << "Analysis low bound bin";
+    // outout << "\n";
+    // outout << analysislowboundbin;
+    // outout << "\n";
+    // outout << "Fraction4L";
+    // outout << "\n";
+    // outout << fraction4l;
+    // outout << "\n";
+
+    outout << "\nMass "; 
+        
+    outout << acceptedCenter; 
+
+    //Finds signalest
+    double signalest = efficiency*crosssection*1000*luminosity*fraction4l;
+    outout << "\nH++ Signal ";
+    outout << signalest;
+    outout << "\n";
+
+    if (param.sumSwitch == 1)
+        oneSum = (signalest + oneSum);
+    if (param.sumSwitch == 2)
+        twoSum = (signalest + twoSum);
+        
 }
+
+void InvariantMassFitScript()
+{
+    //Defines my text file with my events data as eventfilename
+    const string eventfilename = "eventfiles.txt";
+    //Defines my mass text file as massfilename
+    const string massfilename = "masses.txt";
+    //Reads from the file
+    std::ifstream masssrc(massfilename);
+
+    sumOne = 0;
+    sumTwo = 0;
+
+    //Output file
+    std::ofstream out_file;
+    out_file.open("test.txt");
+
+        //Runs the following loop while the file is open
+        while (masssrc)
+        {
+            std::ifstream eventsrc(eventfilename);
+            string massholder;
+            string masslinenotarget;
+
+            //Finds just the mass value and defines it massholder
+            getline(masssrc, massholder, '\t');   
+            //Finds the rest of line and calls it masslinenotarget         
+            getline(masssrc, masslinenotarget);
+            if (massholder.empty())
+                break;
+
+            
+            std::vector<Parameters> eventArray;
+            int eventnumRead = 0;
+
+            eventArray.push_back(Parameters(massholder + '\t' + masslinenotarget));
+            SignalFunction(eventArray.back(), out_file);
+
+            //Runs the following loop while my file is open
+            while (eventsrc) 
+            {
+
+                //Defines a string holder 
+                string holder;
+                //Gets a line from my events file and assigns it to holder
+                getline(eventsrc, holder);
+                //If the line is empty, end the loop
+                if (holder.empty())
+                    break;
+                //Adds the line to my event array in the first 
+                eventArray.push_back(Parameters(massholder + '\t' + holder));
+                ComputationalFunction(eventArray.back(), out_file);
+                
+            }
+
+            if (eventArray.empty())
+                throw std::runtime_error("eventArray empty");
+        
+
+            //Reads my mass value, and then adds the rest of the line onto it, and feeds it into parameter to run it in the signal func
+
+
+            
+
+
+        }
+    out_file.close();
+    outout << "\nSum of One Estimates\t"
+    outout << sumOne;
+    outout << "\nSum of Two Estimates\t"
+    outout << sumTwo;
+}
+
