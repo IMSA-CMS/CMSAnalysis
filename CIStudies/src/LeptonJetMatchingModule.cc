44efeb6752d19a004bf0575416033e1994108228
Zoie Sloneker <zoie.sloneker@gmail.com>
2021-11-29 11:16:53 -0600

    Updating matching module

--- CIStudies/src/LeptonJetMatchingModule.cc
+++ CIStudies/src/LeptonJetMatchingModule.cc
@@ -15,6 +15,11 @@ bool LeptonJetMatchingModule::process()
     matchingPairs.clear();
     std::vector<Particle> genSimParticles(getInput()->getParticles(InputModule::RecoLevel::GenSim).getParticles());
     std::vector<LeptonJet> recoLeptonJets(lepJet->getLeptonJets());
+    std::vector<Particle> lJets;
+    foreach(LeptonJet lJet:recoLeptonJets)
+    {
+      lJets.push_back(lJet);
+    }
 
     genSize += genSimParticles.size();
     // std::cout << "Size of genSimParticles: " << genSimParticles.size() << "\n";
@@ -26,36 +31,7 @@ bool LeptonJetMatchingModule::process()
     Particle nullParticle(nullptr);
     LeptonJet nullJet(nullParticle);
 
-    while (genSimParticles.size() != 0 && recoLeptonJets.size() != 0)
-    {
-        deltaRMin = std::numeric_limits<double>::max(); // Highest possible double, update after every matched pair
-        // MatchingPair dataList(nullptr, nullptr);
-        MatchingPair dataList(nullParticle, nullJet);
-        for (auto &genParticle : genSimParticles)
-        {
-          // candidate.first = genParticle;
-          for (auto &recoJet : recoLeptonJets)
-          {
-            MatchingPair candidate(genParticle, recoJet);
-            // candidate.second = recoJet;
-            double pairDeltaR = findMatchingPairDeltaR(candidate);
-
-            if (pairDeltaR < deltaRMin)
-            {
-              dataList = candidate; // dataList is needed because candidate is not defined outside the for-each loops
-              deltaRMin = pairDeltaR;
-            }
-          }
-        }        
-        if (deltaRMin < deltaRCutoff)
-        {
-          //std::cout << "Match found!" << "\n";
-          matchingPairs.push_back(dataList);
-          recoLeptonJets.erase(std::find(recoLeptonJets.begin(), recoLeptonJets.end(), dataList.second));          
-        }
-        genSimParticles.erase(std::find(genSimParticles.begin(), genSimParticles.end(), dataList.first));
-    }
-    return true;
+    return match(genSimParticles, lJets);
 }
 
 double LeptonJetMatchingModule::findMatchingPairDeltaR(MatchingPair pair)
