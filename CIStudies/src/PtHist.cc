795faddc8eaf21a66c492d217ef4eebb10831f76
Reese <reesejtr@gmail.com>
2021-12-08 10:25:52 -0600

    Fixed calls to getParticles

--- CIStudies/src/PtHist.cc
+++ CIStudies/src/PtHist.cc
@@ -6,14 +6,14 @@ std::vector<double> PtHist::protectedValue(bool typeGenSim) const
 {
   if (typeGenSim)          // typeGenSim == true, so we want the GenSim values
   {
-    auto genParticles = getInput()->getParticles(InputModule::RecoLevel::GenSim);
+    auto genParticles = getInput()->getLeptons(InputModule::RecoLevel::GenSim);
     auto genSimPt = genParticles.getLeadingTransverseMomentum();
     return {genSimPt};
   }
 
   else                     // typeGenSim == false, so we want the Reco values
   {
-    auto recoParticles = getInput()->getParticles(InputModule::RecoLevel::Reco);
+    auto recoParticles = getInput()->getLeptons(InputModule::RecoLevel::Reco);
     auto recoPt = recoParticles.getLeadingTransverseMomentum();
     return {recoPt};
   }
