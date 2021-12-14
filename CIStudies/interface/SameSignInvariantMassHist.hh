24c4256d1208cc385a5999832387c253248dfd04
Peter Dong <pdong@cern.ch>
2021-12-13 21:20:44 -0600

    Cleaned up, removed selector arguments

--- CIStudies/interface/SameSignInvariantMassHist.hh
+++ CIStudies/interface/SameSignInvariantMassHist.hh
@@ -3,19 +3,16 @@
 
 #include "CIAnalysis/CIStudies/interface/GenSimRecoPrototype.hh"
 
-class Selector;
-
 class SameSignInvariantMassHist : public GenSimRecoPrototype
 {
   public:
     SameSignInvariantMassHist(const bool typeSwitch, const std::string& iname, int iNBins, double iminimum, double imaximum, bool iUsingPhi = false, 
-      bool iMultipleMasses = false, std::shared_ptr<Selector> selector = nullptr);
+      bool iMultipleMasses = false);
   protected:
     virtual std::vector<double> protectedValue(bool typeGenSim) const override;
   private:
     bool usingPhi;
     bool multipleMasses;
-    std::shared_ptr<Selector> leptonSelector;
 };
 
 #endif
