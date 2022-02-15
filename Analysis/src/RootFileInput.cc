bbd229606578516445407d87ee3995e6c7d45eaa
Eric Shackelford <eshackelford@imsa.edu>
2022-02-15 10:56:20 -0600

    Made new branch

---
+++ Analysis/src/RootFileInput.cc
@@ -0,0 +1,16 @@
+#include "CIAnalysis/CIStudies/interface/RootFileInput.hh"
+#include "TFile.h"
+
+RootFileInput::RootFileInput(std::string fileName) :
+	file(TFile::Open(fileName.c_str())) 
+	{
+		if(!file)
+		{
+			throw std::runtime_error("Cannot open file!");
+		}
+	}
+
+TH1* RootFileInput::GetInput(std::string name)
+{
+	return dynamic_cast<TH1*>(file->Get(name.c_str()));
+}
