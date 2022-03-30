6c2109c948e49add9b3398405e8be9b35dec9361
Liam Nelson <lnelson@imsa.edu>
2022-03-30 10:53:42 -0500

    Rewrote the simplest no-constructor histogram classes

--- DataCollection/interface/Histograms.hh
+++ DataCollection/interface/Histograms.hh
@@ -3,6 +3,51 @@
 
 #include "GenSimRecoPrototype.hh"
 
+class AllLeptonInvariantMassHist : GenSimRecoPrototype {
+  using GenSimRecoPrototype::GenSimRecoPrototype;
+
+  std::vector<double> protectedValue(InputModule::RecoLevel level)
+  {
+    auto particles = getInput()->getLeptons(level);
+    auto inv = genParticles.calculateAllLeptonInvariantMass();
+    return {inv};
+  }
+};
+
+class InvariantMassHist : GenSimRecoPrototype
+{
+  using GenSimRecoPrototype::GenSimRecoPrototype;
+
+  std::vector<double> protectedValue(InputModule::RecoLevel level)
+  {
+    auto particles = getInput()->getLeptons(level);
+    auto inv = genParticles.getInvariantMass();
+    return {inv};
+  }
+};
+
+class MassResolutionHist : ResolutionPrototype
+{
+  using ResolutionPrototype::ResolutionPrototype;
+
+  double MassResolutionHist::getValue(const ParticleCollection& partColl) const
+  {
+    return partColl.getInvariantMass();
+  }
+};
+
+class OppositeSignInvariantMass : GenSimRecoPrototype
+{
+  using GenSimRecoPrototype::GenSimRecoPrototype;
+
+  std::vector<double> protectedValue(InputModule::RecoLevel level)
+  {
+    auto particles = getInput()->getLeptons(level);
+    auto inv = genParticles.calculateOppositeSignInvariantMass();
+    return {inv};
+  }
+};
+
 class PtHist : GenSimRecoPrototype
 {
   using GenSimRecoPrototype::GenSimRecoPrototype;
@@ -12,6 +57,29 @@ class PtHist : GenSimRecoPrototype
     auto genParticles = getInput()->getLeptons(level);
     auto genSimPt = genParticles.getLeadingTransverseMomentum();
     return {genSimPt};
+  }
+};
+
+class PtResolutionHist : ResolutionPrototype
+{
+  using ResolutionPrototype::ResolutionPrototype;
+
+  double PtResolutionHist::getValue(const ParticleCollection& partColl) const
+  {
+    //std::cerr << "congrats you made it" << std::endl;
+    return partColl.getLeadingTransverseMomentum();
+  }
+};
+
+class ThirdMuonPtHist : GenSimRecoPrototype
+{
+  using GenSimRecoPrototype::GenSimRecoPrototype;
+
+  double ThirdMuonPtHist::protectedValue(InputModule::RecoLevel level)
+  {
+    auto genParticles = getInput()->getLeptons(level);
+    auto Pt = genParticles.getNthHighestPt(3);
+    return {Pt};
   }
 };
 
