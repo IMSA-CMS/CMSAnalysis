c21d47f603ad525ed7783a94cf876a1292de76ca
George Bayliss <calculanator5@gmail.com>
2022-07-22 12:47:36 -0500

    Modified Analyzer for local running, created GenSimAnalysisPlan and other bug fixes.

--- MCGeneration/python/HistGenerator_cfi.py
+++ MCGeneration/python/HistGenerator_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-
-dimuon = cms.EDAnalyzer('Dimuon', #This cannot be found
+def GetDimuon(DimuonType):
+    return cms.EDAnalyzer(DimuonType,
                       genPartsTag=cms.InputTag("genParticles"),
                       genInfoProduct=cms.InputTag("generator"),
                       decayParticlePID=cms.int32(13),
                       debug=cms.int32(0),
-)
-
+    )
