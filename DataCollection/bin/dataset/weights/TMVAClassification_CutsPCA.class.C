cbfc0505c1eb659574e6ca7a9ec3d0dc1057c8e8
Kevin Zhang <kzhang1@imsa.edu>
2022-09-20 23:25:04 -0500

    New weight files after training model a few times

--- DataCollection/bin/dataset/weights/TMVAClassification_CutsPCA.class.C
+++ DataCollection/bin/dataset/weights/TMVAClassification_CutsPCA.class.C
@@ -10,7 +10,7 @@ Method         : Cuts::CutsPCA
 TMVA Release   : 4.2.1         [262657]
 ROOT Release   : 6.24/07       [399367]
 Creator        : kzhang1
-Date           : Sat Sep 17 14:23:57 2022
+Date           : Sun Sep 18 15:35:42 2022
 Host           : Linux cmsbuild02.cern.ch 3.10.0-1160.36.2.el7.x86_64 #1 SMP Wed Jul 21 11:57:15 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
 Dir            : /uscms/homes/k/kzhang1/practice/CMSSW_12_4_3/src/CMSAnalysis/DataCollection/bin
 Training events: 6941
