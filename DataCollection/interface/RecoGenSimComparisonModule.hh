254f7c42a5394b57322acbee352cfc30dda93583
Zhengyu Pan <panzhenyu2014@gmail.com>
2022-09-27 13:01:52 -0500

    RecoGenSimComparisonModule changes

--- DataCollection/interface/RecoGenSimComparisonModule.hh
+++ DataCollection/interface/RecoGenSimComparisonModule.hh
@@ -16,14 +16,34 @@ class RecoGenSimComparisonModule : public AnalysisModule
 
 		virtual bool process() override;
 		virtual void writeAll() override;
+		virtual void finalize() override;
 
 	private:
 	// prints information to the screen
-	void printMatchInfo(const ParticleCollection<Particle>& recoParticles, const ParticleCollection<Particle>& genParticles, std::ostream& output_stream = std::cout) const;
+	void printMatchInfo(const ParticleCollection<Particle>& recoParticles, const ParticleCollection<Particle>& genParticles, std::ostream& output_stream = std::cout);
 
 	int numOfEvents = 0;
 	int eventCounter = 0;
-	int selectedcounter = 0;
+	//per particle counters
+	int recoCounter = 0;
+	int electronCounter = 0;
+	int muonCounter = 0;
+	int matchCounter = 0;
+	int accurateMatchCounter = 0;
+	int accElecMatchCounter = 0;
+	int accMuonMatchCounter = 0;
+	int accHighEtaMatchCounter = 0;
+	int elecMatchCounter = 0;
+	int muonMatchCounter = 0;
+	int highEtaCounter = 0;
+	int highEtaMatchCounter = 0;
+
+	//per event counters
+	int wrongChargeCounter = 0;
+	int fakePhotonCounter = 0;
+	int noMatchCounter = 0;
+	int mismeasuredPtCounter = 0;
+	int accurateEventCounter = 0;
 };
 
 
