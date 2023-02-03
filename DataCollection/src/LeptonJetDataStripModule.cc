f758f6cb1290ae9d57dde85ab38e6c69758c413e
Jesus Fileto <jesus_filetojr@icloud.com>
2023-02-02 20:40:26 -0600

    More Changes to hopefully fix the weird stuff Kevin Z. did

--- DataCollection/src/LeptonJetDataStripModule.cc
+++ DataCollection/src/LeptonJetDataStripModule.cc
@@ -8,18 +8,17 @@
 
 void LeptonJetDataStripModule::initialize()
 {
-
     tree = new TTree("Signal", "Lepton Jet Data");
     tree2 = new TTree("Matched Signal", "Lepton Jet Data");
     for (TTree *treeId : {tree, tree2})
     {
+        // treeId->Branch("jetIndex", &jetIndex, "jetIndex/I");
         treeId->Branch("nParticles", &nParticles, "nParticles/I");
-        treeId->Branch("leading_pt", &leading_pt, "pt/F");
+        treeId->Branch("pt", &pt, "pt/F");
         treeId->Branch("phi", &phi, "phi/F");
         treeId->Branch("eta", &eta, "eta/F");
+        // treeId->Branch("mass", &mass, "mass/F");
         treeId->Branch("deltaR", &deltaR, "deltaR/F");
-        treeId->Branch("sum_pt", &sum_pt, "sum_pt/F");
-        treeId->Branch("delta_pt", &delta_pt, "delta_pt/F");
 
         std::cout << treeId->GetListOfBranches();
     }
@@ -34,6 +33,7 @@ void LeptonJetDataStripModule::writeAll()
 {
     tree->Write();
     tree2->Write();
+    // file->Close();
 }
 
 void LeptonJetDataStripModule::finalize()
@@ -45,13 +45,16 @@ bool LeptonJetDataStripModule::process()
     auto leptonJets = recomod->getLeptonJets();            // inputs from LeptonJet
     auto matchedLeptonJets = matchmod->getMatchingPairs(); // inputs from QCDM2000?
 
+    int jetIterator = 0;
+
     for (const auto &leptonJet : leptonJets)
     {
         nParticles = leptonJet.getNumParticles();
-        leading_pt = 0;
-        double pt2 = 0;
+        pt = leptonJet.getPt();
+        // jetIndex = jetIterator;
         phi = leptonJet.getPhi();
         eta = leptonJet.getEta();
+        // mass = leptonJet.getMass();
         deltaR = 0;
 
         for (Particle p : leptonJet.getParticles())
@@ -62,51 +65,39 @@ bool LeptonJetDataStripModule::process()
                 {
                     deltaR = p.getDeltaR(q);
                 }
-                if (p.getPt() > leading_pt)
-                {
-                    pt2 = leading_pt;
-                    leading_pt = p.getPt();
-                }
             }
         }
-
-        sum_pt = leptonJet.getPt();
-        delta_pt = leading_pt - pt2;
 
         tree->Fill();
+
+        jetIterator++;
     }
 
-    for (const auto &matchedPair : matchedLeptonJets)
+    for (const auto &leptonJet : matchedLeptonJets)
     {
-        auto leptonJet = matchedPair.second;
-        nParticles = leptonJet.getNumParticles();
-        leading_pt = 0;
-        double pt2 = 0;
-        phi = leptonJet.getPhi();
-        eta = leptonJet.getEta();
+        const auto &fil = leptonJet.second;
+        nParticles = fil.getNumParticles();
+        pt = fil.getPt();
+        // jetIndex = jetIterator;
+        phi = fil.getPhi();
+        eta = fil.getEta();
+        // mass = fil.getMass();
         deltaR = 0;
-        pt2 = 0;
 
-        for (Particle p : leptonJet.getParticles())
+        for (Particle p : fil.getParticles())
         {
-            for (Particle q : leptonJet.getParticles())
+            for (Particle q : fil.getParticles())
             {
                 if (p.getDeltaR(q) > deltaR)
                 {
                     deltaR = p.getDeltaR(q);
                 }
-                if (p.getPt() > leading_pt)
-                {
-                    pt2 = leading_pt;
-                    leading_pt = p.getPt();
-                }
             }
         }
 
-        sum_pt = leptonJet.getPt();
-        delta_pt = leading_pt - pt2;
-
         tree2->Fill();
+
+        jetIterator++;
     }
 
     return true;
