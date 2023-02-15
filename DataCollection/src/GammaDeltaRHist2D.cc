8ef844bee91fa15714a02d4abe61266753a97c18
Vikram Rao <vikrao2006@gmail.com>
2023-02-15 15:57:24 -0600

    adding 2D GammaDeltaRHist

--- DataCollection/src/GammaDeltaRHist2D.cc
+++ DataCollection/src/GammaDeltaRHist2D.cc
@@ -5,20 +5,64 @@
 
 #include <vector>
 
-GammaDeltaRHist2D::GammaDeltaRHist2D(const std::string &iname, int iNBinsX, int iNBinsY, double iMinX, double iMinY, double iMaxX, double iMaxY) : 
-    HistogramPrototype2D(iname, iNBinsX, iNBinsY, iMinX, iMinY, iMaxX, iMaxY){};
+GammaDeltaRHist2D::GammaDeltaRHist2D(std::shared_ptr<LeptonJetReconstructionModule> ileptonJetRecoModule, const std::string &iname, int iNBinsX, int iNBinsY, double iMinX, double iMinY, double iMaxX, double iMaxY) : 
+    HistogramPrototype2D(iname, iNBinsX, iNBinsY, iMinX, iMinY, iMaxX, iMaxY),
+    leptonJetRecoModule(ileptonJetRecoModule)
+    {} 
 
 std::vector<std::pair<double, double>> GammaDeltaRHist2D::value2D() const
 
 {
-  std::vector<std::pair<double, double>> error = {{-1,-1}}; 
-  std::vector<std::pair<double, double>> gammaDeltaRvalues;
+  //std::cout<<"entered file" << "\n"; 
 
-  std::vector<double> gamma_leptonJets; 
+  //std::vector<std::pair<double, double>> gammaDeltaRvalues;
 
-  const std::vector<LeptonJet>& LeptonJets = leptonJetRecoModule->getLeptonJets();
+  //std::vector<double> gamma_leptonJets; 
+
+  //new
+  //std::cout << typeid(leptonJetRecoModule).name() << '\n';
+  //std::cout << typeid(*leptonJetRecoModule).name();
+  auto LeptonJets = leptonJetRecoModule->getLeptonJets();
   
-  if (LeptonJets.size() > 0)
+  //std::cout<< "got leptons" << "\n";
+
+  std::vector<std::pair<double, double>> gammaDeltaRpairs;
+
+  uint numJets = LeptonJets.size();
+
+  //std::cerr<< "got num jets" << "\n";
+
+  //for (LeptonJet jet : LeptonJets)
+  for (uint i = 0; i < numJets; i++)
+  {
+    //std::cerr << "in for loop\n";
+    const std::vector<Particle>& particles = LeptonJets[i].getParticles();
+
+    //std::cout<< "particles gamma: " << LeptonJets[i].getGamma() << "\n" << "particles Delta R: " << particles[0].getDeltaR(particles[1]);
+
+    gammaDeltaRpairs.push_back({{LeptonJets[i].getGamma()}, {particles[0].getDeltaR(particles[1])}});
+
+
+    /*for (Particle p : jet.getParticles())
+	  {
+		  for (Particle q : jet.getParticles())
+		  {
+			  //if (p.getDeltaR(q) > deltaR)
+			  //{
+				double deltaR = p.getDeltaR(q);
+
+                std::cout << jet.getGamma();
+                std::cout << deltaR;
+                gammaDeltaRpairs.push_back({{jet.getGamma()}, {deltaR}});
+
+			  //}
+		  }
+	  }*/
+  }
+  //std::cout << "reached end" << "\n";
+  return gammaDeltaRpairs;
+  
+  /*if (LeptonJets.size() > 0)
   {
       for (uint i = 0; i < LeptonJets.size(); i++)
       {
@@ -27,25 +71,24 @@ std::vector<std::pair<double, double>> GammaDeltaRHist2D::value2D() const
   }
   else    
   {
-      return error;
+      return {};
   } 
 
   const std::vector<double>& deltaRValues = leptonJetRecoModule->getDeltaRValues();
 
-  if (deltaRValues.size() > 0)
+  if (deltaRValues.size() > gamma_leptonJets.size())
   {
       for (uint j = 0; j < gamma_leptonJets.size(); j++)
       {
-        //gammaDeltaRvalues.push_back({{gamma_leptonJets[j]}, {deltaRValues[0][j]}})
         gammaDeltaRvalues.push_back({{gamma_leptonJets[j]}, {deltaRValues[j]}});
       }
   }
   else
   {
-      return error;
+      for (uint j = 0; j < deltaRValues.size(); j++)
+      {
+        gammaDeltaRvalues.push_back({{gamma_leptonJets[j]}, {deltaRValues[j]}});
+      }
   }
-  //std::vector<std::pair<double, double>> gammaDeltaRvalues = {{gamma_leptonJets}, {deltaRValues[0]}};
-  //std::vector<std::vector<double>> gammaDeltaRvalues = {{gamma_leptonJets}, {deltaRValues[0]}};
-  
-  return gammaDeltaRvalues;
+  return gammaDeltaRvalues;*/
 };
