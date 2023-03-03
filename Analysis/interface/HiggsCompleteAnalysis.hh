9c30a0ebbbef3408c06938c843d9e533b5db6cea
Francis Cianciolo <fcianciolo@imsa.edu>
2023-03-03 10:47:27 -0600

    Added SimpleEstimator

--- Analysis/interface/HiggsCompleteAnalysis.hh
+++ Analysis/interface/HiggsCompleteAnalysis.hh
@@ -12,6 +12,7 @@ class HiggsCompleteAnalysis : public FullAnalysis
         HiggsCompleteAnalysis();
         std::shared_ptr<Channel> getChannel(std::string name) override;
 	    std::vector<std::shared_ptr<Channel>> getChannels() {return channels;}
+        TH1* getHiggsHist(std::string histType, bool scaleToExpected = false) const;
 
     private:
         std::vector<std::shared_ptr<Channel>> channels;
