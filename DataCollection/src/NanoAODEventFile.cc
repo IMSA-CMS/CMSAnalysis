d731990d6c18d72569c101a879c47e2bd3692370
Dean Barrow <dean33261@gmail.com>
2023-03-07 15:59:59 -0600

    hello

--- DataCollection/src/NanoAODEventFile.cc
+++ DataCollection/src/NanoAODEventFile.cc
@@ -19,6 +19,7 @@ std::vector<bool> NanoAODEventFile::getTriggerResults(std::string subProcess) co
 
     for(auto& trigger : triggers)
     {
+        // std::cout << "Is this for loop working" << "\n";
         v_results.push_back(*(trigger.second));
     }
 
@@ -39,6 +40,8 @@ std::vector<std::string> NanoAODEventFile::getTriggerNames(std::string subProces
 
 bool NanoAODEventFile::checkTrigger(std::string triggerName, std::string subProcess) const
 {
+    // add cout statement to check, it in fact does pass through this code
+    // std::cout << "Does this Trigger work?" << "\n";
     return *(triggers.find(triggerName)->second);
 }
 
@@ -111,16 +114,18 @@ NanoAODEventFile::NanoAODEventFile(TFile *ifile) :
     //initializing triggers from header file
     std::ifstream triggerNameFile("betterValidTriggers.txt");
 
-    if(!triggerNameFile)
+    if(triggerNameFile)
     {
         std::string nameoftrigger;
 
         while(getline(triggerNameFile, nameoftrigger))
         {
+            std::cout << nameoftrigger << "Does this work?" << "\n";
             if(tree->GetBranch(nameoftrigger.c_str()))
             {
                 TTreeReaderValue<Bool_t> intermediate(treeReader, nameoftrigger.c_str());
                 triggers.emplace(nameoftrigger, intermediate);
+                std::cout << nameoftrigger <<"\n";
             }
         }
     }    
@@ -130,11 +135,11 @@ NanoAODEventFile::NanoAODEventFile(TFile *ifile) :
 }
 
 void NanoAODEventFile::nextEvent()
-{
+{ 
     treeReader.Next(); 
     setEventCount(getEventCount() + 1);
 
-    if(getVariable<UInt_t>("gen_size") > 0)
+    if(variables.find("gen_size") != variables.end() && getVariable<UInt_t>("gen_size") > 0)
     {
         genSimParticles.clear();
         genSimParticles.reserve(getVariable<UInt_t>("gen_size") );
