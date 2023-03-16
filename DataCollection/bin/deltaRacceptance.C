79e236b3af79956b02581f3867f66cac11bd7161
Vikram Rao <vikrao2006@gmail.com>
2023-03-16 14:06:27 -0500

    fixing acceptance plot

--- DataCollection/bin/deltaRacceptance.C
+++ DataCollection/bin/deltaRacceptance.C
@@ -24,12 +24,12 @@ void deltaRacceptance()
 
 
   //get the histograms from the root file
-  TH1* alldeltaRHist=dynamic_cast<TH1*>(allGenSimdeltaR->Get("Jet Detected_hists/Jet Detected Gen Sim Delta R")); 
-  TH1* deltaRfilteredHist=dynamic_cast<TH1*>(genSimdeltaRaccepted->Get("No Jets_hists/No Jets Gen Sim Delta R")); 
+  TH1* alldeltaRHist=dynamic_cast<TH1*>(allGenSimdeltaR->Get("Gen Sim Delta R")); 
+  TH1* deltaRfilteredHist=dynamic_cast<TH1*>(genSimdeltaRaccepted->Get("Gen Sim Delta R (W/ Reconstructed Jets)")); 
   
   // scale the pass trigger histogram 
   
-  std::cerr << " Accpetence Ratio: " << (deltaRfilteredHist->GetEntries() / alldeltaRHist->GetEntries() ) << std::endl;
+  //std::cerr << " Accpetence Ratio: " << (deltaRfilteredHist->GetEntries() / alldeltaRHist->GetEntries() ) << std::endl;
 
   //passTriggerHist->Scale((allEventsHist->GetEntries()) / (allEventsHist->Integral()*passTriggerHist->GetEntries())); 
   //passTriggerHist->Scale((passTriggerHist->GetEntries()*allEventsHist->Integral()) / (passTriggerHist->Integral()*allEventsHist->GetEntries())); //makes ratios match, doesn't fix hists
@@ -42,12 +42,12 @@ void deltaRacceptance()
   //triggerEfficiencyHist->Divide(passTriggerHist, allEventsHist);
 
   
-  deltaRacceptanceHist->Divide(alldeltaRHist);
+  deltaRfilteredHist->Divide(alldeltaRHist);
   
-  deltaRacceptanceHist->GetXaxis()->SetTitle("Delta R");
-  deltaRacceptanceHist->GetYaxis()->SetTitle("Percentage of Events Accepted");
-  deltaRacceptanceHist->SetTitle("Delta R Acceptance");
-  deltaRacceptanceHist->Draw();
+  deltaRfilteredHist->GetXaxis()->SetTitle("Delta R");
+  deltaRfilteredHist->GetYaxis()->SetTitle("Percentage of Events Accepted");
+  deltaRfilteredHist->SetTitle("Delta R Acceptance");
+  deltaRfilteredHist->Draw();
 }*/
 
 
@@ -73,32 +73,19 @@ void deltaRacceptance()
 
 using std::string;
 
-TH1* getHist(string name, TDirectory* directory);
+TH1* getHist(string name, TFile* file);
 void process(TH1* hist, std::string canvasName, std::string xaxis, std::string yaxis);
 
 void deltaRacceptance()
 {
-  
     TFile* dRHists = TFile::Open("test.root");
     
-    TDirectory* detected = (TDirectory*)dRHists->Get("Jet Detected_hists;1");
-    TDirectory* no_hists = (TDirectory*)dRHists->Get("No Jets_hists;1");
+    string filteredHist = "Gen Sim Delta R (W/ Reconstructed Jets)";
+    string allHist = "Gen Sim Delta R";
     
-    string filteredHist = "Jet Detected Gen Sim Delta R";
-    string allHist = "No Jets Gen Sim Delta R";
-    
-    detected->cd();
-    
-    auto filteredPlot = getHist(filteredHist.c_str(), detected);
-
-    dRHists->cd();
-    no_hists->cd();
-
-    auto allPlot =  getHist(allHist.c_str(), no_hists);
-
-    dRHists->cd();
-
-    auto acceptedHist = (TH1D*) filteredPlot->Clone();
+    auto filteredPlot = getHist(filteredHist.c_str(), dRHists);
+    auto allPlot =  getHist(allHist.c_str(), dRHists);
+    auto acceptedHist = (TH1D*) filteredPlot->Clone(); 
     acceptedHist->Divide(allPlot);
 
     string canvasName = "Delta R Acceptance";
@@ -108,9 +95,9 @@ void deltaRacceptance()
 }
 
 
-TH1* getHist(std::string name, TDirectory* directory)
+TH1* getHist(std::string name, TFile* file)
     {
-    auto hist = dynamic_cast<TH1*>(directory->Get(name.c_str()));
+    auto hist = dynamic_cast<TH1*>(file->Get(name.c_str()));
     if (!hist)
         throw std::runtime_error(name + " not found!");
     return hist;
