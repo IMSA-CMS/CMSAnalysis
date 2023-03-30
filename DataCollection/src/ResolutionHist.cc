c28f7fc4bf24ad6c2a3b9b81fad3f413128c0b31
Vikram Rao <vikrao2006@gmail.com>
2023-03-30 16:14:44 -0500

    fixed ResolutionHist

--- DataCollection/src/ResolutionHist.cc
+++ DataCollection/src/ResolutionHist.cc
@@ -6,34 +6,29 @@
 #include "CMSAnalysis/DataCollection/interface/LeptonJetMatchingModule.hh"
 #include <iostream>
 
-ResolutionHist::ResolutionHist(std::shared_ptr<LeptonJetMatchingModule> iLeptonJetMatchingModule, const std::string& iname, int iNBins, double iminimum, double imaximum):
+ResolutionHist::ResolutionHist(std::shared_ptr<LeptonJetMatchingModule> ileptonJetMatchModule, const std::string& iname, int iNBins, double iminimum, double imaximum):
     HistogramPrototype1D(iname, iNBins, iminimum, imaximum),
-    LeptonJetMatchingModule(iLeptonJetMatchingModule),
+    leptonJetMatchModule(ileptonJetMatchModule)
 {
+} 
 
-}
-
-std::vector<double> ResolutionHist::value() const
+std::vector<double> ResolutionHist::value() const 
 { 
   //pair of particles <gen sim, reco>
-  const auto matchedPairs = LeptonJetMatchingModule->getMatchingPairs();
-  std::vector<double> gammaVector{};
-  std::vector<std::pair<double, double>> ResolutionVector{};
+  const auto& matchedPairs = leptonJetMatchModule->getMatchingPairs();
+  std::vector<std::pair<double, double>> gammaVector{};
+  std::vector<double> ResolutionVector{};
   for (auto matchedPair : matchedPairs)
   { 
-    if (analysis=="Gamma")
-    {
-        gammaVector.push_back(matchedPair.first().getGamma(), matchedPair.second().getGamma());
-    }
-    else 
-    {
-        std::runtime_error("analysis incorectly inputed");
-    }
+    gammaVector.push_back(std::make_pair(matchedPair.first.getGamma(), matchedPair.second.getGamma()));
+    //std::cout << "Gen Sim Gamma: " << matchedPair.first.getGamma() << "Reco Gamma: " << matchedPair.second.getGamma() << "\n";
   }
   for (auto gammaPair : gammaVector)
   {
-    auto resolution = (gammaPair.first()-gammaPair.second())/(gammaPair.first());
+    auto resolution = (gammaPair.first-gammaPair.second)/(gammaPair.first);
+    std::cout<< "Resolution: " << resolution << "\n";
     ResolutionVector.push_back(resolution);
   }
   return ResolutionVector;
-  }
+}
+ 
