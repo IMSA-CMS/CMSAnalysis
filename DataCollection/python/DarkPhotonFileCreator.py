41f7bc8613327324e0427f981894ebabab5fb8fb
Rohan Jain <rjain@imsa.edu>
2023-05-08 12:57:41 -0500

    added textfiles for new Dark Photon samples

---
+++ DataCollection/python/DarkPhotonFileCreator.py
@@ -0,0 +1,51 @@
+import os 
+import subprocess
+
+f = open('python/hzdp.txt', 'r')
+
+for line in f.readlines():
+	NAME = 'DarkPhoton/DarkPhoton_'
+	line = line.strip()
+
+	if 'Run3' in line:
+		continue
+
+	output = str(subprocess.check_output('dasgoclient -query="file dataset=' + line + '"', shell=True))
+	output = output[2:len(output)-3]
+	output = output.replace('\\n', "\n")
+
+	decay = 'Decay_HiggsDPZ'
+	NAME += decay + '_'
+
+	DPMass = line[int(line.index("DPMass") + 7) : int(line.index("DPMass") + 10)]
+	DPMass = DPMass.replace('p', '_')
+	NAME += 'DpMass_' + DPMass + '_'
+
+	# only first one
+	# FSR = line[int(line.index("FSR", 40, 100) + 4) : int(line.index("FSR", 40, 100) + 7)]
+	# FSR = FSR.replace('p', '_')
+	# NAME += 'FSR_' + FSR + '_'
+	# del
+
+	NAME += "Format_MiniAOD_"
+	HMass = line[int(line.index("-", line.index("HMass"), 100) + 1) : int(line.index("_", line.index("HMass"), 100))]
+	HMass = HMass.replace('p', '_')
+	NAME += "HiggsMass_" + HMass + "_"
+
+	PERIOD = 'Period_'
+	if 'APV' in line:
+		PERIOD += "2016APV"
+	elif "20UL16" in line:
+		PERIOD += "2016"
+	elif "20UL17" in line:
+		PERIOD += "2017"
+	elif "20UL18" in line:
+		PERIOD += "2018"
+
+	NAME += PERIOD
+	NAME += "_Run_2.txt"
+	
+	newFile = open('bin/textfiles/' + NAME, 'w')
+	newFile.write(output)
+	newFile.close()
+	# print(NAME)
