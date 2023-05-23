86e85f2937260eca7b45c433751e7821606eee9d
Surya Ninad Bhamidi <snbhamidi@gmail.com>
2023-05-23 16:00:48 -0500

    Made Changes to HiggsInvariantMassModule

--- DataCollection/src/HiggsCutsSelector.cc
+++ DataCollection/src/HiggsCutsSelector.cc
@@ -8,16 +8,21 @@
 
 void HiggsCutsSelector::selectParticles(const InputModule* input, Event& event)
 {
+    std::cout << "Adding particles\n";
     auto particles = input->getLeptons(InputModule::RecoLevel::Reco);
     for(auto particle : particles) 
     {
-        if(particle.getType() == ParticleType::muon()) 
-        {
-            event.addMuon(particle);
-        }
-        else if(particle.getType() == ParticleType::electron()) 
-       	{
-           event.addElectron(particle);
+        // if (particle.getPt() > 5)
+        // {
+            if(particle.getType() == ParticleType::muon()) 
+            {
+                // std::cout << "Adding muons of pt " << particle.getPt() << '\n';
+                event.addMuon(particle);
+            }
+            else if(particle.getType() == ParticleType::electron()) 
+            {
+            event.addElectron(particle);
+            // }
         }
     }
 }
