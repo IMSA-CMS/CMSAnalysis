d8ac5d0731e904464221fe4e1cc3b31a29d3ec74
Ivan Chen <ichen2@imsa.edu>
2023-10-04 16:06:50 -0500

    Revert "tested three lepton pt cuts"
    This reverts commit 4109ffe57c017c6f169366fd61d28ce514e88942.

--- Filters/src/HiggsSelector.cc
+++ Filters/src/HiggsSelector.cc
@@ -10,148 +10,36 @@
 
 void HiggsSelector::selectParticles(const EventInput* input, Event& event) const
 {
-    std::vector<Particle> selected;
-    // std::vector<Particle> posElecs(0);
-    // std::vector<Particle> negElecs(0);
-    // std::vector<Particle> posMuons(0);
-    // std::vector<Particle> negMuons(0);
     auto particles = input->getLeptons(EventInput::RecoLevel::Reco).getParticles();
 
-    // int electronCount = 0;
-    // int posMuonCount = 0;
-    // int negMuonCount = 0;
-    
-    
     for (const auto& particle : particles)
     {
+        // std::cout << "Electron ID: " << ParticleType::electron().getpdgId() << '\n';
         if (particle.getType() == ParticleType::electron()) 
 		{
-            if(Lepton(particle).isLoose()  
+            auto lepton = Lepton(particle);
+            // std::cout << "Particle type: " << particle.getType().getName() << '\n';
+            if(lepton.isLoose()
                 && particle.getPt() > 5 
-                // && particle.getInfo("CutBasedHEEP")
+                && particle.getInfo("CutBasedHEEP")
             )
             {
+                // std::cout << "Call CutBasedHEEP \n";
                 event.addElectron(particle);
-
             }
-            
-            // electronCount++;
-
-            // if (particle.getCharge() > 0) {
-            //     posElecs.push_back(Particle(particle));
-            // } 
-            // else {
-            //     negElecs.push_back(Particle(particle));
-            // }
         }
-        // if (particle.getType()s == Particle::Type::Muon && particle.getPt() > 40 && std::abs(particle.getEta()) < 2.8) 
-		if (particle.getType() == ParticleType::muon())
+        else if (particle.getType() == ParticleType::muon())
         {
-            if(Lepton(particle).isLoose()  
+            auto lepton = Lepton(particle);
+            if (lepton.isLoose()  
                 && particle.getPt() > 5 
-                // && particle.getInfo("Isolation") < 0.05
+                && particle.getInfo("Isolation") < 0.025
+                && particle.getDXY() < 0.025
+                && particle.getDZ() < 0.0625
             )
             {
-
                 event.addMuon(particle);
-            }
-            // if (particle.getCharge() > 0) {
-            //     posMuonCount++;
-            //     posMuons.push_back(Particle(particle));
-            // } 
-            // else {
-            //     negMuonCount++;
-            //     negMuons.push_back(Particle(particle));
-            // }
+            }            
         }
     }
-    
-    // int index1 = 0;
-    // int index2 = 0;
-    // double largestMass = 0.0f;
-    // if (posElecs.size() > 0) 
-    // {
-    //     for (int i = 0; i < int(posElecs.size()) - 1; i++) 
-    //     {
-    //         for (int j = i+1; j < int(posElecs.size()); j++)
-    //         {
-    //             if (posElecs[i].getType() == posElecs[j].getType() && (posElecs[i].getFourVector() + posElecs[j].getFourVector()).M() > largestMass)
-    //             {
-    //                 index1 = i;
-    //                 index2 = j;
-    //                 largestMass = (posElecs[i].getFourVector() + posElecs[j].getFourVector()).M();
-                    
-    //             }
-    //         }
-    //     }
-    //     selected.push_back(posElecs[index1]);
-    //     selected.push_back(posElecs[index2]);
-    // }
-    // index1 = 0;
-    // index2 = 0;
-    // largestMass = 0.0f;
-    // if (negElecs.size() > 0)
-    // {
-    //     for (int i = 0; i < int(negElecs.size()) - 1; i++) 
-    //     {
-    //         for (int j = i+1; j < int(negElecs.size()); j++)
-    //         {
-    //             if (negElecs[i].getType() == negElecs[j].getType() && (negElecs[i].getFourVector() + negElecs[j].getFourVector()).M() > largestMass)
-    //             {
-    //                 index1 = i;
-    //                 index2 = j;
-    //                 largestMass = (negElecs[i].getFourVector() + negElecs[j].getFourVector()).M();
-    //             }
-    //         }
-    //     }
-    //     selected.push_back(negElecs[index1]);
-    //     selected.push_back(negElecs[index2]);
-    // }
-
-    // index1 = 0;
-    // index2 = 0;
-
-    // if (posMuonCount > 0)
-    // {
-    //     for (int i = 0; i < posMuonCount - 1; i++) 
-    //     {
-    //         for (int j = i + 1; j < posMuonCount; j++)
-    //         {
-    //             if (posMuons[i].getType() == posMuons[j].getType() && (posMuons[i].getFourVector() + posMuons[j].getFourVector()).M() > largestMass)
-    //             {
-    //                 index1 = i;
-    //                 index2 = j;
-    //                 largestMass = (posMuons[i].getFourVector() + posMuons[j].getFourVector()).M();
-                    
-    //             }
-    //         }
-    //     }
-    //     selected.push_back(posMuons[index1]);
-    //     selected.push_back(posMuons[index2]);
-    // }
-    // index1 = 0;
-    // index2 = 0;
-    // largestMass = 0.0f;
-    // if (negMuonCount > 0)
-    // {
-    //     for (int i = 0; i < negMuonCount-1; i++) 
-    //     {
-    //         for (int j = i+1; j < negMuonCount; j++)
-    //         {
-    //             if (negMuons[i].getType() == negMuons[j].getType() && (negMuons[i].getFourVector() + negMuons[j].getFourVector()).M() > largestMass)
-    //             {
-    //                 index1 = i;
-    //                 index2 = j;
-    //                 largestMass = (negMuons[i].getFourVector() + negMuons[j].getFourVector()).M();
-    //             }
-    //         }
-    //     }
-    //     selected.push_back(negMuons[index1]);
-    //     selected.push_back(negMuons[index2]);
-    // }
-    // if (posMuonCount + negMuonCount < 4 && electronCount < 4)
-    // {
-    //     return std::vector<Particle>(0);
-    // }
-
 }
