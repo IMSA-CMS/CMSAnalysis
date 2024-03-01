40ef6ba76041433554543b1c08c4f2e964d3efb7
Shrikar Dulam <shrikar.dulam@gmail.com>
2024-03-01 16:13:02 -0600

    Testing Implemented PdfAnalysis

--- Modules/interface/PdfAnalysisModule.hh
+++ Modules/interface/PdfAnalysisModule.hh
@@ -1,24 +1,27 @@
-#ifndef AFBMODULE_HH
-#define AFBMODULE_HH
+#ifndef PDFANALYSISMODULE_HH
+#define PDFANALYSISMODULE_HH
 
 #include "HistogramOutputModule.hh"
 
 
 // AFBModule fills forward and backward histograms for both gen sim and reco and uses those histograms to calculate the value of Afb at the end
-class AFBModule : public HistogramOutputModule
+class PdfAnalysisModule : public HistogramOutputModule 
 {
 public:
-  AFBModule(int low = 300, int high = 3200, int histBins = 1000);
+  PdfAnalysisModule(int low = 0, int high= 1000, int histBins = 1000);
 
   virtual void initialize() override;
   virtual bool process() override;
   virtual void finalize() override;
 
 private:
-  int low;
-  int high;
-  // the forward and backward histograms are cut in this # of bins
-  const int histBins = 1000;
+  Double_t low;
+  Double_t high;
+  //# of bins
+  Int_t histBins = 1000;
+  std::vector<TH1*> histVec;
+  std::vector<std::vector<double>> histogramBinRatios;
+  TH1* defHist;
 
 };
 
