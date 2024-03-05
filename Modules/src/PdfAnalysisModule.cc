8fbb6ddd0a961a86ddd93a39c6fe241821a94146
Shrikar Dulam <shrikar.dulam@gmail.com>
2024-03-05 14:32:08 -0600

    Fixed PdfAnalysis

--- Modules/src/PdfAnalysisModule.cc
+++ Modules/src/PdfAnalysisModule.cc
@@ -31,17 +31,13 @@ void PdfAnalysisModule::initialize()
 
 void PdfAnalysisModule::finalize()
 {
-  std::cout << "PdfAnalysisModule Finalize Started" << std::endl;
   for(int i = 0; i < 100; ++i){
     auto hist = histVec[i];
     std::vector<double> binRatios;
-    std::cout << "Filling RatiosHistogram " << i << std::endl;
     for(int j = 0; j < histBins; ++j){
-      std::cout << "At Bin: " << j << std::endl;
       auto binContent = hist->GetBinContent(j);
       double binRatio = binContent/(defHist->GetBinContent(j));
       binRatios.push_back(binRatio);
-      std::cout << "Added Ratio" << std::endl;
       //Some problem here as well 
    
     }
@@ -55,19 +51,24 @@ void PdfAnalysisModule::finalize()
 
 bool PdfAnalysisModule::process()
 {
-  
+
   auto recoParticles = getInput()->getLeptons(EventInput::RecoLevel::Reco);
+  if(recoParticles.size() == 0)
+  {
+    std::cout << "No Reco Particles" << std::endl;
+    return true;
+  }
   //auto recoCS = recoParticles.getCollinsSoper();
   auto recoInvMass = recoParticles.getInvariantMass();
   defHist->Fill(recoInvMass);
   auto& particle = recoParticles[0];
   //No idea how to get particle info
-  for(int i = 0; i < 100; ++i){
+  for(int i = 0; i < 100; ++i)
+  {
     // 
     auto pdfWeight = particle.getInfo("PDFWeight"+std::to_string(i));
     auto hist = histVec[i];
     hist->Fill(recoInvMass, pdfWeight);
   }
-  std::cout << "PdfAnalysisModule Process Ended" << std::endl;
   return true;
 }
