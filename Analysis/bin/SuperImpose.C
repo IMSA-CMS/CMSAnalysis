7e14827957ac77ae356a84796c127b2cae5e2ad9
Shrikar Dulam <shrikar.dulam@gmail.com>
2024-03-23 17:28:57 -0500

    Fixed SuperImpose for PDFWeight Histograms

--- Analysis/bin/SuperImpose.C
+++ Analysis/bin/SuperImpose.C
@@ -16,22 +16,27 @@
 #include "TStyle.h"
 
 void SuperImpose() {
-    //Change extraText here
+
+    //Change extraText here (for watermarking purposes)
     auto plotFormatter = std::make_shared<PlotFormatter>(false, "Preliminary");
-    //Change the filePath here. This should be the longest branch all input files have in common.
-    const std::string filePath = "/uscms/home/vrao/analysis/CMSSW_12_4_3/src/CMSAnalysis/DataCollection/bin/";
-    //Write the remaining file paths and graphs here. The hist in index 0 of the hists vector gets pulled from the file at index 0 in files, and so on.
-    //Write your graph names here (for the legend)
-    
+
+    //Change the filePath here. This should be the longest branch all input files have in common. CHANGE USERNAME TO YOUR USERNAME
+    const std::string filePath = "/uscms/home/sdulam/analysis/CMSSW_12_4_3/src/CMSAnalysis/";
+
+    //Pick source files for histograms here
     std::vector<std::string> files = {"testpdfbackground.root"};
+
+    //Write the remaining file paths and graphs here. The hist in index 0 of the hists vector gets pulled from the file at index 0 in files, and so on.
     std::vector<std::string> hists = {"DefaultHistogram", "LowHistogram", "HighHistogram"};
+    //Write your graph names here (for the legend)
     std::vector<TString> names = {"Default", "Low Weight", "High Weight"};
+    
 
     //Colors go here
     std::vector<int> colors = {1, 2, 3, 4, 5};
     //Change x and y axis titles here
-    TString xTitle = "Bins";
-    TString yTitle = "Same-sign Dilepton Mass (GeV)";
+    TString xTitle = "Same-sign Dilepton Mass (GeV)";
+    TString yTitle = "Weights";
 
 
     int count = 0;
@@ -42,23 +47,27 @@ void SuperImpose() {
     for(std::string file : files) {
 	    fileName = filePath + file; 
         openedFile = TFile::Open(fileName.c_str(), "read");
-        if(!openedFile) {
-            throw std::runtime_error("Cannot open file!");
-            }
-        hist = dynamic_cast<TH1*>(openedFile->Get(hists.at(count).c_str()));
-        if (!hist)
+        if(!openedFile) 
         {
-            throw std::runtime_error("Histogram " + hists.at(count) + " not found!");
+            throw std::runtime_error("Cannot open file!");
         }
-        if(dynamic_cast<TH2 *>(hist) != 0) {
-            TH2* hist2D = dynamic_cast<TH2 *>(hist);
-            TH1 *newhist = hist2D->ProjectionX("_px", 0, -1, "E");
-            histVector.push_back(newhist);
+        for(int i = 0; i < hists.size(); i++){
+
+            hist = dynamic_cast<TH1*>(openedFile->Get(hists.at(i).c_str()));
+            if (!hist)
+            {
+                throw std::runtime_error("Histogram " + hists.at(i) + " not found!");
+            }
+            if(dynamic_cast<TH2 *>(hist) != 0) {
+                TH2* hist2D = dynamic_cast<TH2 *>(hist);
+                TH1 *newhist = hist2D->ProjectionX("_px", 0, -1, "E");
+                histVector.push_back(newhist);
+            }
+            else {
+                histVector.push_back(hist);
+            }
         }
-        else {
-            histVector.push_back(hist);
-        }
-        count++;
     }
+    std::cout << "SuperImpose Finished Iteration " << histVector.size() <<std::endl;
     TCanvas *canvas = plotFormatter->simpleSuperImposedHist(histVector, colors, names, xTitle, yTitle);
 }
