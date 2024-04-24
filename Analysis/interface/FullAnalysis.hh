fdf576ef8318680a2b188987b66b96fe29f3398c
Ivan Chen <ichen2@imsa.edu>
2024-04-24 13:21:36 -0500

    Got rid of weird lines in parameter fits

--- Analysis/interface/FullAnalysis.hh
+++ Analysis/interface/FullAnalysis.hh
@@ -19,7 +19,16 @@ class FullAnalysis
         //To my knowledge, this has to be defined in derived classes to use that class' variables
         virtual std::shared_ptr<Channel> getChannel(std::string name) = 0;
         virtual std::vector<std::shared_ptr<Channel>> getChannels() = 0;
+        /**
+         * Get histograms method
+         * @param histType The variable the histogram should show
+         * @param processName The specific process that formed the events, like Signal, Drell-Yan, ZZ, TTbar, etc
+         * @param channelName Name of the Channel class to get the process from
+         * @return Returns a list of histograms ordered by mass from lowest to highest
+        */
+        virtual std::vector<TH1*> getHistograms(const std::string& histType, const std::string& processName, const std::string& channel, bool scaleToExpected = false) = 0;
         virtual TH1* getDecayHist(std::string histType, std::string processName, double massTarget, bool scaleToExpected = false) const = 0;
+
         //Process makers for easy use when loading channels
         static SingleProcess makeSingleProcess(std::vector<HistVariable> histVariables, std::vector<HistVariable> fitHistVariables, std::string filePathway, std::string fileName, std::string fitFileName, std::string crossSectionName, std::shared_ptr<CrossSectionReader> crossReader, int massTarget, double luminosity, std::vector<std::shared_ptr<Correction>> corrections = {});
         static SingleProcess makeSignalProcess(std::vector<HistVariable> histVariables, std::string filePathway, std::string fileName, std::string crossSectionName, std::shared_ptr<CrossSectionReader> crossReader, int massTarget, double luminosity, std::vector<std::shared_ptr<Correction>> corrections = {});
--- Analysis/interface/HiggsCompleteAnalysis.hh
+++ Analysis/interface/HiggsCompleteAnalysis.hh
@@ -12,6 +12,7 @@ class HiggsCompleteAnalysis : public FullAnalysis
         HiggsCompleteAnalysis();
         std::shared_ptr<Channel> getChannel(std::string name) override;
 	    std::vector<std::shared_ptr<Channel>> getChannels() override {return channels;}
+        std::vector<TH1*> getHistograms(const std::string& histType, const std::string& processName, const std::string& channel, bool scaleToExpected = false) override;
         TH1* getDecayHist(std::string histType, std::string processName, double massTarget, bool scaleToExpected = false) const override;
 
     private:
