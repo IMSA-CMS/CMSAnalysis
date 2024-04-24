ea82b6644a5c190e70129bd55bc97bae71448540
Malcolm Wilson-Ahlstrom <mwilson@imsa.edu>
2024-04-24 14:56:49 -0500

    stuff

--- Plans/src/LeptonJetReconstructionPlan.cc
+++ Plans/src/LeptonJetReconstructionPlan.cc
@@ -83,7 +83,7 @@ void LeptonJetReconstructionPlan::initialize()
   //$$$auto recoGenSimComparisonMod = std::make_shared<RecoGenSimComparisonModule>();
   auto leptonJetMLStripMod = std::make_shared<LeptonJetMLStripModule>();
   leptonJetMLStripMod->setInput(eventMod->getEventInput());
-  auto mlMod = std::make_shared<MLCalculator>(leptonJetMLStripMod, "dataset/weights/TMVAClassification_BDT.weights.xml", "BDT");
+  // auto mlMod = std::make_shared<MLCalculator>(leptonJetMLStripMod, "dataset/weights/TMVAClassification_BDT.weights.xml", "BDT");
 
   // Histograms
   //uncomented 
@@ -111,7 +111,7 @@ void LeptonJetReconstructionPlan::initialize()
 
   auto lepJetRecoElec = std::make_shared<LeptonJetRecoHist>(lepMatchMod, true, lepRecoMod, "Number of Reconstructed Electrons",3,0,2);
   auto lepJetRecoMuon = std::make_shared<LeptonJetRecoHist>(lepMatchMod, false, lepRecoMod, "Number of Reconstructed Muons",3,0,2);
-  auto leptonJetMLHist = std::make_shared<LeptonJetMLHist>(EventInput::RecoLevel::Reco, "LeptonJetMLOutput", 100, -.4, .4, mlMod, lepRecoMod);
+  //auto leptonJetMLHist = std::make_shared<LeptonJetMLHist>(EventInput::RecoLevel::Reco, "LeptonJetMLOutput", 100, -.4, .4, mlMod, lepRecoMod);
 
   // auto matchDeltaRHist = std::make_shared<MatchingDeltaRHist>(lepMatchMod, "Differences in Delta R for Matched Lepton Jets", 100, 0, 0.5);
   // auto matchPtHist = std::make_shared<MatchingPtHist>(lepMatchMod, "Differences in pT for Matched Lepton Jets", 100, -300, 300);
@@ -136,7 +136,7 @@ void LeptonJetReconstructionPlan::initialize()
   histOutputMod->addHistogram(deltaXYHist);
   histOutputMod->addHistogram(deltaZHist);
   //histOutputMod->addHistogram(relIsoHist);
-  histOutputMod->addHistogram(leptonJetMLHist);
+  //histOutputMod->addHistogram(leptonJetMLHist);
 
   //   histOutputMod->addHistogram(matchDeltaRHist);
   //   histOutputMod->addHistogram(matchPtHist);
@@ -204,7 +204,7 @@ void LeptonJetReconstructionPlan::initialize()
   // modules.addProductionModule(genPartMod);
   modules.addProductionModule(lepMatchMod);
   modules.addProductionModule(leptonJetMLStripMod);
-  modules.addProductionModule(mlMod);
+  //modules.addProductionModule(mlMod);
 
 
   modules.addProductionModule(eventMod);
