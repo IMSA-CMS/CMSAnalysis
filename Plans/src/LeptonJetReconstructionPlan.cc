f319449c9155f18a5cf2da23d99ab955bdd33803
Ivan Chen <ichen2@imsa.edu>
2024-04-25 13:44:17 -0500

    Got error bars for H++ DSCB norm parameter

--- Plans/src/LeptonJetReconstructionPlan.cc
+++ Plans/src/LeptonJetReconstructionPlan.cc
@@ -73,11 +73,10 @@ void LeptonJetReconstructionPlan::initialize()
   //auto triggerCut = make_shared<TriggerCut>(std::vector<std::string>{"HLT_IsoMu24"});
   eventMod->addCut(triggerCut);
   auto matchMod = std::make_shared<MatchingModule>();
-  //auto lepRecoMod = std::make_shared<LeptonJetReconstructionModule>(.5);
+  auto lepRecoMod = std::make_shared<LeptonJetReconstructionModule>(.5);
   auto genPartMod = std::make_shared<GenSimParticleModule>(1000022);
   auto eventDumpMod = std::make_shared<EventDumpModule>(true,true);
-  auto lepMatchMod =
-      std::make_shared<LeptonJetMatchingModule>(lepRecoMod, 0.1); // this
+  auto lepMatchMod = std::make_shared<LeptonJetMatchingModule>(lepRecoMod, 0.1); // this
   auto histOutputMod = std::make_shared<HistogramOutputModule>();
   //auto mlMod = std::make_shared<LeptonJetMLCalculator>();
   //$$$auto recoGenSimComparisonMod = std::make_shared<RecoGenSimComparisonModule>();
