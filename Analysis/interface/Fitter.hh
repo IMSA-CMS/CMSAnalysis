dffc57649c331d6d3333bebbd439c62c0e6430e2
Ivan Chen <ichen2@imsa.edu>
2024-05-20 09:53:36 -0500

    Got Higgs signal

--- Analysis/interface/Fitter.hh
+++ Analysis/interface/Fitter.hh
@@ -1,12 +1,12 @@
 #ifndef FITTER_HH
 #define FITTER_HH
 
-#include <unordered_map>
+#include <map>
 #include <TH1.h>
 #include <TCanvas.h>
 #include <TFile.h>
+#include <TGraph.h>
 #include "CMSAnalysis/Analysis/interface/FitFunctionCollection.hh"
-
 
 struct ParameterizationData
 {
@@ -45,16 +45,28 @@ public:
 	void loadFunctions(const FitFunctionCollection& fitFunctions);
 	// void loadHistogram(const std::vector<string>& histNames);
 
+	TFitResultPtr fitFunction(TF1* function, TH1* hist);
+	TFitResultPtr fitFunction(TF1* function, TGraph* graph);
+
 	void fitFunctions();
 	void parameterizeFunctions(std::unordered_map<std::string, double>& xData, const std::vector<std::string>& paramNames);
 private:
 	TCanvas* fitExpressionFormula(TH1* histogram, FitFunction& fitFunction);
 	TCanvas* fitDSCB(TH1* histogram, FitFunction& fitFunction);
 	TCanvas* fitPowerLaw(TH1* histogram, FitFunction& fitFunction);
+	
+	//Insert blank TF1* function ptr which will have the fitted function written to
+	static TFitResultPtr functionFittingLoop(TGraph* graph, TF1* function);
+	static TFitResultPtr fitSingleFunction(TGraph* histogram, TF1* function, size_t iterations = 1);
 
 	// TH1* readHistogram(const std::string& name);
 	std::vector<ParameterizationData> getParameterData(std::unordered_map<std::string, double>& xData);
 	FitFunction parameterizeFunction(ParameterizationData& parameterData);
+
+	//Different ways to try and get seed function for fitting
+	//Gets guess inverse power law function y = a(x-b)^-c + d using three points, only guarantees the powerlaw through p0 and p1, not p2
+	static TF1* seedInversePowerLaw(double x_0, double y_0, double x_1, double y_1, double x_2, double y_2);
+	static std::vector<TF1*> trialFitFunctions;
 };
 
 #endif
--- Analysis/src/SimpleEstimator.cc
+++ Analysis/src/SimpleEstimator.cc
@@ -21,12 +21,10 @@ double SimpleEstimator::getExpectedYield(const SingleProcess* process, std::stri
     //}
 
     int totalEventsInt = process->getTotalEvents();
-    std::cout << "1\n";
     double totaleventsran = totalEventsInt;
 
     //Finds crosssection (from spreadsheet)
     double crosssection = process->getCrossSection();
-    std::cout << "2\n";
 
 
 //without integral code
@@ -47,7 +45,6 @@ double SimpleEstimator::getExpectedYield(const SingleProcess* process, std::stri
     //std::cout << "numBins: " << numBins << "\n";
     int lowerLimit = firstBin * (static_cast<double>(numBins) / xAxisRange);
     double efficiency = (hist->Integral(lowerLimit, numBins)) / totaleventsran;
-    std::cout << "3\n";
     double signalest = crosssection * 1000 * luminosity * efficiency;
 // */
 /*
