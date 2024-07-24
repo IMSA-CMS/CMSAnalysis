298b50fa6598cf427e82619af4f609cc0cf732f3
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-07-23 20:11:48 -0500

    punzi criterion for bdt

--- DataCollection/bin/MLTrain.C
+++ DataCollection/bin/MLTrain.C
@@ -35,229 +35,236 @@ dy50: 5711,
 qcd2000: 6.458
 */
 
-bool returnState(TString &myMethodList)
-{
+bool returnState(TString &myMethodList) {
+  int test = 3;
+  test++;
+  bool isReal;
 
-    bool isReal;
+  TMVA::Tools::Instance();
 
-    TMVA::Tools::Instance();
+  std::map<std::string, int> Use;
 
-    std::map<std::string, int> Use;
+  // Cut optimisation
+  Use["Cuts"] = 0;
+  Use["CutsD"] = 0;
+  Use["CutsPCA"] = 0;
+  Use["CutsGA"] = 0;
+  Use["CutsSA"] = 0;
 
-    // Cut optimisation
-    Use["Cuts"] = 0;
-    Use["CutsD"] = 0;
-    Use["CutsPCA"] = 0;
-    Use["CutsGA"] = 0;
-    Use["CutsSA"] = 0;
+  // Neural Networks (all are feed-forward Multilayer Perceptrons)
+  Use["MLP"] = 1;       // Recommended ANN
+  Use["MLPBFGS"] = 0;   // Recommended ANN with optional training method
+  Use["MLPBNN"] = 0;    // Recommended ANN with BFGS training method and bayesian regulator
+  Use["CFMlpANN"] = 0;  // Depreciated ANN from ALEPH
+  Use["TMlpANN"] = 0;   // ROOT's own ANN
 
-    // Neural Networks (all are feed-forward Multilayer Perceptrons)
-    Use["MLP"] = 1;      // Recommended ANN
-    Use["MLPBFGS"] = 0;  // Recommended ANN with optional training method
-    Use["MLPBNN"] = 0;   // Recommended ANN with BFGS training method and bayesian regulator
-    Use["CFMlpANN"] = 0; // Depreciated ANN from ALEPH
-    Use["TMlpANN"] = 0;  // ROOT's own ANN
-
-    // Boosted Decision Trees
-    Use["BDT"] = 1;  // Boosted Decision Trees (Adaptive Boost, recommended)
-    Use["BDTG"] = 0; // uses Gradient Boost
-    Use["BDTB"] = 0; // uses Bagging
-    Use["BDTD"] = 0; // decorrelation + Adaptive Boost
-    Use["BDTF"] = 0; // allow usage of fisher discriminant for node splitting
+  // Boosted Decision Trees
+  Use["BDT"] = 1;   // Boosted Decision Trees (Adaptive Boost, recommended)
+  Use["BDTG"] = 0;  // uses Gradient Boost
+  Use["BDTB"] = 0;  // uses Bagging
+  Use["BDTD"] = 0;  // decorrelation + Adaptive Boost
+  Use["BDTF"] = 0;  // allow usage of fisher discriminant for node splitting
 
 #ifdef R__HAS_TMVAGPU
-    Use["DNN_GPU"] = 0; // CUDA-accelerated DNN training.
+  Use["DNN_GPU"] = 0;  // CUDA-accelerated DNN training.
 #else
-    Use["DNN_GPU"] = 0;
+  Use["DNN_GPU"] = 0;
 #endif
 
 #ifdef R__HAS_TMVACPU
-    Use["DNN_CPU"] = 0; // Multi-core accelerated DNN.
+  Use["DNN_CPU"] = 0;  // Multi-core accelerated DNN.
 #else
-    Use["DNN_CPU"] = 0;
+  Use["DNN_CPU"] = 0;
 #endif
 
-    std::cout << std::endl;
-    std::cout << "==> Start TMVAClassification" << std::endl;
+  std::cout << std::endl;
+  std::cout << "==> Start TMVAClassification" << std::endl;
 
-    // Select methods (don't look at this code - not of interest)
-    if (myMethodList != "")
-    {
+  // Select methods (don't look at this code - not of interest)
+  if (myMethodList != "") {
+    for (std::map<std::string, int>::iterator it = Use.begin(); it != Use.end(); it++)
+      it->second = 0;
+
+    std::vector<TString> mlist = TMVA::gTools().SplitString(myMethodList, ',');
+    for (UInt_t i = 0; i < mlist.size(); i++) {
+      std::string regMethod(mlist[i]);
+
+      if (Use.find(regMethod) == Use.end()) {
+        std::cout << "Method \"" << regMethod
+                  << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
         for (std::map<std::string, int>::iterator it = Use.begin(); it != Use.end(); it++)
-            it->second = 0;
-
-        std::vector<TString> mlist = TMVA::gTools().SplitString(myMethodList, ',');
-        for (UInt_t i = 0; i < mlist.size(); i++)
-        {
-            std::string regMethod(mlist[i]);
-
-            if (Use.find(regMethod) == Use.end())
-            {
-                std::cout << "Method \"" << regMethod << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
-                for (std::map<std::string, int>::iterator it = Use.begin(); it != Use.end(); it++)
-                    std::cout << it->first << " ";
-                std::cout << std::endl;
-                return 1;
-            }
-            Use[regMethod] = 1;
-        }
+          std::cout << it->first << " ";
+        std::cout << std::endl;
+        return 1;
+      }
+      Use[regMethod] = 1;
     }
+  }
 
-    // input signal file here
-    std::string sgFile = "strippedSG_numFiles1.root";
+  // input signal file here
+  std::string sgFile = "strippedSG_numFiles1.root";
 
-    // input background files here
-    std::string bgFiles[] =
-        {
-            "strippedBG_numFiles1.root"
-        };
+  // input background files here
+  std::string bgFiles[] = {"strippedBG_numFiles1.root"};
 
-    // (it is also possible to use ASCII format as input -> see TMVA Users Guide)
-    TFile *input(0);
-    TString fname = "./tmva_class_example.root";
-    if (!gSystem->AccessPathName(fname))
-    {
-        input = TFile::Open(fname); // check if file in local directory exists
+  // (it is also possible to use ASCII format as input -> see TMVA Users Guide)
+  TFile *input(0);
+  TString fname = "./tmva_class_example.root";
+  if (!gSystem->AccessPathName(fname)) {
+    input = TFile::Open(fname);  // check if file in local directory exists
+  } else {
+    TFile::SetCacheFileDir(".");
+    input = TFile::Open(sgFile.c_str(), "CACHEREAD");  //
+  }
+  if (!input) {
+    std::cout << "ERROR: could not open data file" << std::endl;
+    exit(1);
+  }
+  std::cout << "--- TMVAClassification       : Using input file: " << input->GetName() << std::endl;
+
+  // Registers background files in background chain
+  TTree *bg = new TTree();
+  TChain *backgroundTree = new TChain("Signal");  // Signal of background files
+  for (std::string file : bgFiles) {
+    backgroundTree->Add(file.c_str());
+  }
+
+  TTree *signalTree = (TTree *)input->Get("Signal");
+
+  // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
+  TString outfileName("TMVA.root");
+  TFile *outputFile = TFile::Open(outfileName, "RECREATE");
+
+  TMVA::Factory *factory =
+      new TMVA::Factory("TMVAClassification",
+                        outputFile,
+                        "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification");
+
+  TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset");
+
+  // change var1 to index and var2 to pt
+  LeptonJetMLStripModule leptonMod;
+  leptonMod.initialize();
+  leptonMod.addVariablesToDataLoader(dataloader);
+  // Difference in transverse momentum between leading and runner up highest transverse momentum particles.
+
+  // global event weights per tree (see below for setting event-wise weights)
+  Double_t signalWeight = 1.0;
+  Double_t backgroundWeight = 1.0;
+  // You can add an arbitrary number of signal or background trees
+  dataloader->AddSignalTree(signalTree, signalWeight);
+  dataloader->AddBackgroundTree(backgroundTree, backgroundWeight);
+
+  // I am using all of the data reduction methods
+  if (Use["Cuts"])
+    factory->BookMethod(
+        dataloader, TMVA::Types::kCuts, "Cuts", "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart");
+
+  if (Use["CutsD"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kCuts,
+                        "CutsD",
+                        "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=Decorrelate");
+
+  if (Use["CutsPCA"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kCuts,
+                        "CutsPCA",
+                        "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=PCA");
+
+  if (Use["CutsGA"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kCuts,
+                        "CutsGA",
+                        "H:!V:FitMethod=GA:CutRangeMin[0]=-10:CutRangeMax[0]=10:VarProp[1]=FMax:EffSel:Steps=30:Cycles="
+                        "3:PopSize=400:SC_steps=10:SC_rate=5:SC_factor=0.95");
+
+  if (Use["CutsSA"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kCuts,
+                        "CutsSA",
+                        "!H:!V:FitMethod=SA:EffSel:MaxCalls=150000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-"
+                        "6:Eps=1e-10:UseDefaultScale");
+
+  if (Use["MLP"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kMLP,
+                        "MLP",
+                        "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator");
+
+  if (Use["BDT"])
+    factory->BookMethod(dataloader,
+                        TMVA::Types::kBDT,
+                        "BDT",
+                        "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:"
+                        "UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20");
+
+  // Using both CUDA-Accelerated and Multi-core Accelerated Deep Neural Networks
+  if (Use["DNN_CPU"] or Use["DNN_GPU"]) {
+    // General layout
+
+    TString layoutString("Layout=TANH|128,TANH|128,TANH|128,LINEAR");
+
+    // Define Training strategy. One could define multiple strategy string separated by the "|" delimiter
+
+    TString trainingStrategyString =
+        ("TrainingStrategy=LearningRate=1e-2,Momentum=0.9,"
+         "ConvergenceSteps=20,BatchSize=100,TestRepetitions=1,"
+         "WeightDecay=1e-4,Regularization=None,"
+         "DropConfig=0.0+0.5+0.5+0.5");
+
+    // General Options
+    TString dnnOptions(
+        "!H:V:ErrorStrategy=CROSSENTROPY:VarTransform=N:"
+        "WeightInitialization=XAVIERUNIFORM");
+    dnnOptions.Append(":");
+    dnnOptions.Append(layoutString);
+    dnnOptions.Append(":");
+    dnnOptions.Append(trainingStrategyString);
+
+    // Cuda implementation
+    if (Use["DNN_GPU"]) {
+      TString gpuOptions = dnnOptions + ":Architecture=GPU";
+      factory->BookMethod(dataloader, TMVA::Types::kDL, "DNN_GPU", gpuOptions);
     }
-    else
-    {
-        TFile::SetCacheFileDir(".");
-        input = TFile::Open(sgFile.c_str(), "CACHEREAD"); //
+    // Multi-core CPU implementation
+    if (Use["DNN_CPU"]) {
+      TString cpuOptions = dnnOptions + ":Architecture=CPU";
+      factory->BookMethod(dataloader, TMVA::Types::kDL, "DNN_CPU", cpuOptions);
     }
-    if (!input)
-    {
-        std::cout << "ERROR: could not open data file" << std::endl;
-        exit(1);
-    }
-    std::cout << "--- TMVAClassification       : Using input file: " << input->GetName() << std::endl;
+  }
 
-    // Registers background files in background chain
-    TTree *bg = new TTree();
-    TChain *backgroundTree = new TChain("Signal"); // Signal of background files
-    for (std::string file : bgFiles)
-    {
-        backgroundTree->Add(file.c_str());
-    }
+  // Train MVAs using the set of training events
+  factory->TrainAllMethods();
 
-    TTree *signalTree = (TTree *)input->Get("Signal");
+  // Evaluate all MVAs using the set of test events
+  factory->TestAllMethods();
 
-    // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
-    TString outfileName("TMVA.root");
-    TFile *outputFile = TFile::Open(outfileName, "RECREATE");
+  // Evaluate and compare performance of all configured MVAs
+  factory->EvaluateAllMethods();
 
-    TMVA::Factory *factory = new TMVA::Factory("TMVAClassification", outputFile,
-                                               "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification");
+  // Save the output
+  outputFile->Close();
 
-    TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset");
+  std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
+  std::cout << "==> TMVAClassification is done!" << std::endl;
 
-    // change var1 to index and var2 to pt
-    LeptonJetMLStripModule leptonMod; 
-    leptonMod.initialize();
-    leptonMod.addVariablesToDataLoader(dataloader);
-    // Difference in transverse momentum between leading and runner up highest transverse momentum particles.
+  TFile *f = new TFile("TMVARerun.root", "recreate");
+  factory->Write();
+  f->Close();
 
-    // global event weights per tree (see below for setting event-wise weights)
-    Double_t signalWeight = 1.0;
-    Double_t backgroundWeight = 1.0;
-    // You can add an arbitrary number of signal or background trees
-    dataloader->AddSignalTree(signalTree, signalWeight);
-    dataloader->AddBackgroundTree(backgroundTree, backgroundWeight);
+  delete factory;
+  delete dataloader;
 
-    // I am using all of the data reduction methods
-    if (Use["Cuts"])
-        factory->BookMethod(dataloader, TMVA::Types::kCuts, "Cuts",
-                            "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart");
-
-    if (Use["CutsD"])
-        factory->BookMethod(dataloader, TMVA::Types::kCuts, "CutsD",
-                            "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=Decorrelate");
-
-    if (Use["CutsPCA"])
-        factory->BookMethod(dataloader, TMVA::Types::kCuts, "CutsPCA",
-                            "!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=PCA");
-
-    if (Use["CutsGA"])
-        factory->BookMethod(dataloader, TMVA::Types::kCuts, "CutsGA",
-                            "H:!V:FitMethod=GA:CutRangeMin[0]=-10:CutRangeMax[0]=10:VarProp[1]=FMax:EffSel:Steps=30:Cycles=3:PopSize=400:SC_steps=10:SC_rate=5:SC_factor=0.95");
-
-    if (Use["CutsSA"])
-        factory->BookMethod(dataloader, TMVA::Types::kCuts, "CutsSA",
-                            "!H:!V:FitMethod=SA:EffSel:MaxCalls=150000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale");
-
-    if (Use["MLP"])
-        factory->BookMethod(dataloader, TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator");
-
-    if (Use["BDT"])
-        factory->BookMethod(dataloader, TMVA::Types::kBDT, "BDT",
-                            "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20");
-
-    // Using both CUDA-Accelerated and Multi-core Accelerated Deep Neural Networks
-    if (Use["DNN_CPU"] or Use["DNN_GPU"])
-    {
-        // General layout
-
-        TString layoutString("Layout=TANH|128,TANH|128,TANH|128,LINEAR");
-
-        // Define Training strategy. One could define multiple strategy string separated by the "|" delimiter
-
-        TString trainingStrategyString = ("TrainingStrategy=LearningRate=1e-2,Momentum=0.9,"
-                                          "ConvergenceSteps=20,BatchSize=100,TestRepetitions=1,"
-                                          "WeightDecay=1e-4,Regularization=None,"
-                                          "DropConfig=0.0+0.5+0.5+0.5");
-
-        // General Options
-        TString dnnOptions("!H:V:ErrorStrategy=CROSSENTROPY:VarTransform=N:"
-                           "WeightInitialization=XAVIERUNIFORM");
-        dnnOptions.Append(":");
-        dnnOptions.Append(layoutString);
-        dnnOptions.Append(":");
-        dnnOptions.Append(trainingStrategyString);
-
-        // Cuda implementation
-        if (Use["DNN_GPU"])
-        {
-            TString gpuOptions = dnnOptions + ":Architecture=GPU";
-            factory->BookMethod(dataloader, TMVA::Types::kDL, "DNN_GPU", gpuOptions);
-        }
-        // Multi-core CPU implementation
-        if (Use["DNN_CPU"])
-        {
-            TString cpuOptions = dnnOptions + ":Architecture=CPU";
-            factory->BookMethod(dataloader, TMVA::Types::kDL, "DNN_CPU", cpuOptions);
-        }
-    }
-
-    // Train MVAs using the set of training events
-    factory->TrainAllMethods();
-
-    // Evaluate all MVAs using the set of test events
-    factory->TestAllMethods();
-
-    // Evaluate and compare performance of all configured MVAs
-    factory->EvaluateAllMethods();
-
-    // Save the output
-    outputFile->Close();
-
-    std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
-    std::cout << "==> TMVAClassification is done!" << std::endl;
-
-    TFile *f = new TFile("TMVAfactory.root", "recreate");
-    factory->Write();
-    f->Close();
-
-    delete factory;
-    delete dataloader;
-
-    // Launch the GUI for the root macros
-    if (!gROOT->IsBatch())
-        TMVA::TMVAGui(outfileName);
-    return false;
+  // Launch the GUI for the root macros
+  if (!gROOT->IsBatch())
+    TMVA::TMVAGui(outfileName);
+  return false;
 }
 
-void MLTrain() // int argc, char** argv)
+void MLTrain()  // int argc, char** argv)
 {
-    // Select methods (don't look at this code - not of interest)
-    TString methodList;
-    returnState(methodList);
+  // Select methods (don't look at this code - not of interest)
+  TString methodList;
+  returnState(methodList);
 }
