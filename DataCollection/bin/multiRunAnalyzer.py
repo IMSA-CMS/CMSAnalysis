0176fc14450ad13c1fc3199a6ce7978681a177f0
Max Chen <pandamaxchen@gmail.com>
2024-08-02 11:15:05 -0500

    Better plots

--- DataCollection/bin/multiRunAnalyzer.py
+++ DataCollection/bin/multiRunAnalyzer.py
@@ -109,7 +109,9 @@ if __name__ == '__main__':
 
 	# List of jobs to run on from those above
 	# jobsList = [ttBar, zz, dy50, multiBoson, higgsSignal, higgsData] if analysis == 0 or analysis == 2 else [darkPhotonSignal]
-	jobsList = [higgsSignal] if analysis == 0 or analysis == 2 else [darkPhotonSignal]
+	
+	#jobsList = [higgsSignal] if analysis == 0 or analysis == 2 else [darkPhotonSignal]
+	jobsList = [qcd]
 	
 	if os.path.exists("nohup.out") and (len(sys.argv) <= 2 or sys.argv[2] != "keep"):
 		os.remove("nohup.out")
