34be584174d9511f1d075a4f6a96b0afe966aa60
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-08-09 11:40:24 -0500

    fixed linker errors the stupid way

--- Histograms/interface/MLStripHist.hh
+++ Histograms/interface/MLStripHist.hh
@@ -2,38 +2,19 @@
 #define MLSTRIPHIST_HH
 
 #include "CMSAnalysis/Utility/interface/HistogramPrototype1D.hh"
-#include "CMSAnalysis/Utility/interface/LeptonJet.hh"
 #include <functional>
 #include <string>
-#include <vector>
 
 class MLStripHist : public HistogramPrototype1D
 {
 public:
-    MLStripHist(const std::string& iname, int iNBins, double iminimum, double imaximum, std::function<std::vector<double>(LeptonJet)> func);
+    MLStripHist(const std::string& iname, int iNBins, double iminimum, double imaximum, std::function<std::vector<double>()> func)
+        : HistogramPrototype1D(iname, iNBins, iminimum, imaximum), MLStripFunction(func) {}
+    
+    std::vector<double> value() const override { return MLStripFunction(); }
 
-    void setLeptonJet(LeptonJet jet);
-
-    virtual std::vector<double> value() const override;
-
-protected:
-    LeptonJet histJet;
-    std::function<std::vector<double>(LeptonJet)> MLStripFunction;
+private:
+    std::function<std::vector<double>()> MLStripFunction;
 };
-
-// Inline function definitions
-
-inline MLStripHist::MLStripHist(const std::string& iname, int iNBins, double iminimum, double imaximum, std::function<std::vector<double>(LeptonJet)> func)
-    : HistogramPrototype1D(iname, iNBins, iminimum, imaximum), MLStripFunction(func) {}
-
-inline void MLStripHist::setLeptonJet(LeptonJet jet)
-{
-    histJet = jet;
-}
-
-inline std::vector<double> MLStripHist::value() const
-{
-    return MLStripFunction(histJet);
-}
 
 #endif // MLSTRIPHIST_HH
