23682bdd273ff51d6e91647a6641094967e332bc
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-08-21 23:52:28 -0500

    full mu pt analysis

--- Analysis/bin/multiSuperPlot.sh
+++ Analysis/bin/multiSuperPlot.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# varNames=("Eta" "Leading Pt" "nParticles" "Delta R")
-varNames=("1st Highest mu- Pt" "1st Highest Lepton Jet Pt" "Input Delta Pt Values" "Input Leading Pt Values")
+# Check if the fileList variable is passed as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <fileList>"
+    exit 1
+fi
 
-# Define the fileList variable
-fileList="RecoConfig1_DPTrigger_MuPtCut_Inputs"
+# Assign the first argument to the fileList variable
+fileList="$1"
+
+# Define the varNames array
+varNames=("1st Highest mu- Pt" "1st Highest Lepton Jet Pt" "Input Delta Pt Values" "Input Leading Pt Values")
 
 # Define the list of regions and their corresponding abbreviations
 declare -A regions
@@ -13,7 +19,7 @@ regions["High Mass and Same Sign"]="HMSS"
 regions["Low Mass and Same Sign"]="LMSS"
 # Add more regions and abbreviations as needed
 
-# Loop through each Eta value
+# Loop through each variable name
 for varName in "${varNames[@]}"; do
     # Loop through each region and its abbreviation
     for region in "${!regions[@]}"; do
@@ -22,7 +28,6 @@ for varName in "${varNames[@]}"; do
         outputFile="${fileList}_$(echo $varName | tr -d ' ')_${abbreviation}.root"
         
         # Run the ROOT macro
-        # root -q "SuperPlot.C(\"Input ${varName} Values ${region}\", \"MultiSuperPlots/superplot_${outputFile}\")"
-        root -q "SuperPlot.C(\"${varName} ${region}\", \"MultiSuperPlots/superplot_${outputFile}\")"
+        root -q "/uscms/home/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/SuperPlot.C(\"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Output/${fileList}/\", \"${varName} ${region}\", \"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/MuPtAnalysis/superplot_${outputFile}\")"
     done
 done
