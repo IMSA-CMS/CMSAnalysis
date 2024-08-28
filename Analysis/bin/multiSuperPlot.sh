37b642afad1eeb537006756ca9a26ef020f8bbf3
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-08-28 14:59:34 -0500

    added highest mu- pt analysis

--- Analysis/bin/multiSuperPlot.sh
+++ Analysis/bin/multiSuperPlot.sh
@@ -10,24 +10,32 @@ fi
 fileList="$1"
 
 # Define the varNames array
-varNames=("1st Highest mu- Pt" "1st Highest Lepton Jet Pt" "Input Delta Pt Values" "Input Leading Pt Values")
+#varNames=("1st Highest mu- Pt" "1st Highest Lepton Jet Pt" "Input Delta Pt Values" "Input Leading Pt Values")
+varNames=("1st Highest mu- Pt" "1st Highest mu- Eta" "2nd Highest mu- Pt" "2nd Highest mu- Eta" "mu- Opposite Sign Invariant Mass" "1st Highest Lepton Jet Pt" "1st Highest Lepton Jet Eta" "1st Highest Lepton Jet Lepton Jet Delta R")
 
 # Define the list of regions and their corresponding abbreviations
 declare -A regions
-regions["High Mass and Different Sign"]="HMOS"
-regions["High Mass and Same Sign"]="HMSS"
-regions["Low Mass and Same Sign"]="LMSS"
+# regions["High Mass and Different Sign"]="HMOS"
+# regions["High Mass and Same Sign"]="HMSS"
+# regions["Low Mass and Same Sign"]="LMSS"
 # Add more regions and abbreviations as needed
 
 # Loop through each variable name
-for varName in "${varNames[@]}"; do
-    # Loop through each region and its abbreviation
-    for region in "${!regions[@]}"; do
-        abbreviation=${regions[$region]}
-        # Construct the output filename
-        outputFile="${fileList}_$(echo $varName | tr -d ' ')_${abbreviation}.root"
+# for varName in "${varNames[@]}"; do
+#     # Loop through each region and its abbreviation
+#     for region in "${!regions[@]}"; do
+#         abbreviation=${regions[$region]}
+#         # Construct the output filename
+#         outputFile="${fileList}_$(echo $varName | tr -d ' ')_${abbreviation}.root"
         
-        # Run the ROOT macro
-        root -q "/uscms/home/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/SuperPlot.C(\"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Output/${fileList}/\", \"${varName} ${region}\", \"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/MuPtAnalysis/superplot_${outputFile}\")"
-    done
+#         # Run the ROOT macro
+#         root -q "/uscms/home/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/SuperPlot.C(\"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Output/${fileList}/\", \"${varName} ${region}\", \"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/MuPtAnalysisNT/superplot_${outputFile}\")"
+#     done
+# done
+for varName in "${varNames[@]}"; do
+    # Construct the output filename
+    outputFile="${fileList}_$(echo $varName | tr -d ' ').root"
+    
+    # Run the ROOT macro
+    root -q "/uscms/home/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/SuperPlot.C(\"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Output/${fileList}/\", \"${varName}\", \"/uscms/homes/j/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Analysis/bin/MuPtAnalysisNT/superplot_${outputFile}\")"
 done
--- DataCollection/bin/multiMuPtAnalysis.sh
+++ DataCollection/bin/multiMuPtAnalysis.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-for i in {28..28}
+for i in $(seq 30 10 100)
 do
     echo "Current Mu pT cut: $i"
-    pathName="DarkPhoton_MLEval_MuPtCut_$i/"
-    pathNameStr="DarkPhoton_MLEval_MuPtCut_$i"
+    pathName="DarkPhoton_MLEval_MuPtCut_NT_$i/"
+    pathNameStr="DarkPhoton_MLEval_MuPtCut_NT_$i"
     echo "Output Path: Output/$pathName"
 
     echo $i > /uscms/home/jpalamad/analysis/CMSSW_14_0_4/src/CMSAnalysis/Filters/src/highestMuonPtCut.txt # populate txt file with current pt cut
 
-    python3 multiRunAnalyzer.py DPBackground keep $pathName
-    python3 multiRunAnalyzer.py DarkPhoton keep $pathName
-    nohup runAnalyzer input=Data/Data_Trigger_SingleMuon_Year_2018A.txt output=${pathName}Data_Trigger_SingleMuon_Year_2018A.root analysis=LeptonJetReconstruction numFiles=1 > nohup.out
+    # python3 multiRunAnalyzer.py DPBackground keep $pathName
+    # python3 multiRunAnalyzer.py DarkPhoton keep $pathName
+    # nohup runAnalyzer input=Data/Data_Trigger_SingleMuon_Year_2018A.txt output=${pathName}Data_Trigger_SingleMuon_Year_2018A.root analysis=LeptonJetReconstruction numFiles=1 > nohup.out
 
-    dy=(
-        "Drell-Yan/Drell-Yan_MassCut_10-50_Run_2.txt"
-        "Drell-Yan/Drell-Yan_MassCut_50-inf_Run_2.txt"
-    )
+    # dy=(
+    #     "Drell-Yan/Drell-Yan_MassCut_10-50_Run_2.txt"
+    #     "Drell-Yan/Drell-Yan_MassCut_50-inf_Run_2.txt"
+    # )
 
-    echo "Beginning Drell-Yan"
+    # echo "Beginning Drell-Yan"
 
-    for file in "${dy[@]}"; do
-        outputFile="${file%.txt}.root"
-        outputFile="${outputFile/Drell-Yan\//}"  # Remove Drell-Yan/ from the output filename if necessary
-        runAnalyzer input="$file" output="${pathNameStr}${outputFile}" analysis=LeptonJetReconstruction numFiles=3 > nohup.out
-    done
-
+    # for file in "${dy[@]}"; do
+    #     outputFile="${file%.txt}.root"
+    #     outputFile="${outputFile/Drell-Yan\//}"  # Remove Drell-Yan/ from the output filename if necessary
+    #     runAnalyzer input="$file" output="${pathNameStr}${outputFile}" analysis=LeptonJetReconstruction numFiles=2 > nohup.out
+    # done
     
-    echo "Completed Drell-Yan"
+    # echo "Completed Drell-Yan"
 
-    command_name="runAnalyzer"
+    # command_name="runAnalyzer"
 
-    # Loop until the command is no longer running
-    while pgrep -x "$command_name" > /dev/null
-    do
-        sleep 5  # Wait for 5 seconds before checking again
-    done
+    # # Loop until the command is no longer running
+    # while pgrep -x "$command_name" > /dev/null
+    # do
+    #     sleep 5  # Wait for 5 seconds before checking again
+    # done
 
-    echo "$command_name has finished. Beginning replacement"
+    # echo "$command_name has finished. Beginning replacement"
 
-    runAnalyzer input=QCD/QCD_HTCut_1500-2000_Run_2_Year_2018.txt output=${pathName}QCD_HTCut_1500-2000_Run_2_Year_2018.root analysis=LeptonJetReconstruction numFiles=2 > nohup.out
+    # runAnalyzer input=QCD/QCD_HTCut_1500-2000_Run_2_Year_2018.txt output=${pathName}QCD_HTCut_1500-2000_Run_2_Year_2018.root analysis=LeptonJetReconstruction numFiles=2
+    runAnalyzer input=ZZ/ZZ_Decay_2e2tau_Run_2.txt output=${pathName}ZZ_Decay_2e2tau_Run_2.root numFiles=5 analysis=LeptonJetReconstruction outputEvery=1000
 
     echo "Replacement completed. Beginning next iteration"
 
