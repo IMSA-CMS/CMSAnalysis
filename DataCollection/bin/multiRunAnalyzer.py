43da470c5bcaf7407fafa66cb4f039c6eb3db7a7
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-08-28 14:57:37 -0500

    added plainqcd to multirunanalyzer

--- DataCollection/bin/multiRunAnalyzer.py
+++ DataCollection/bin/multiRunAnalyzer.py
@@ -8,17 +8,17 @@ analysis = 0
 def loopRun(*fileList):
 
 	if len(sys.argv) <= 3:
-		path = "Higgs/" if analysis == 0 else "DarkPhoton_NoTriggerNoCut/" if analysis == 1 or analysis == 3 or analysis == 4 or analysis == 5 or analysis == 6 else "Muon/" if analysis == 2 else ""
+		path = "Higgs/" if analysis == 0 else "DarkPhoton_NoTriggerYesCut_BQCD/" if analysis == 1 or analysis == 3 or analysis == 4 or analysis == 5 or analysis == 6 or analysis == 7 else "Muon/" if analysis == 2 else ""
 	else:
 		path = sys.argv[3]
 
 	os.makedirs(os.environ['CMSSW_BASE'] + "/src/CMSAnalysis/Output/" + path, exist_ok=True)
 
-	analysisBackground = "HiggsBackground" if analysis == 0 else "LeptonJetReconstruction" if analysis == 1 or analysis == 3 or analysis == 4 or analysis == 5 or analysis == 6 else "Muon" if analysis == 2 else ""
+	analysisBackground = "HiggsBackground" if analysis == 0 else "LeptonJetReconstruction" if analysis == 1 or analysis == 3 or analysis == 4 or analysis == 5 or analysis == 6 or analysis == 7 else "Muon" if analysis == 2 else ""
 	#analysisBackground = "MLVariables"
 
 	# get rid of numFiles for a full run-through
-	numFiles = "numFiles=1"
+	numFiles = "numFiles=2"
 	for file in fileList:
 		# Filling in the parameters of runAnalyzer
 		analysisSignal = "HiggsSignal" if analysis == 0 else "MuonSignal" if analysis == 2 else ""
@@ -68,6 +68,9 @@ if __name__ == '__main__':
 		elif analysisName == "DPData":
 			print("Running DPData Analysis")
 			analysis = 6
+		elif analysisName == "PlainQCD":
+			print("Running PlainQCD Analysis")
+			analysis = 7
 		else:
 			print("Argument did not match any analysis, defaulting to Higgs")
 			analysis = 0
@@ -145,6 +148,30 @@ if __name__ == '__main__':
 		"QCD/QCD_HTCut_2000-inf_Run_2_Year_2018.txt"
 	)
 
+	plainQCD = (
+		"plainQCD/plainQCD_HTCut_PSWeights_100-200_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_1000-1500_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_1500-2000_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_200-300_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_2000-Inf_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_300-500_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_50-100_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_500-700_Run_2_Year_2017.txt",
+		"plainQCD/plainQCD_HTCut_PSWeights_700-1000_Run_2_Year_2017.txt",
+	)
+
+	bQCD = (
+		"BQCD/PlainQCD_HTCut_100-200_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_1000-1500_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_1500-2000_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_200-300_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_2000-Inf_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_300-500_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_50-100_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_500-700_Run_2_Year_2017.txt",
+		"BQCD/PlainQCD_HTCut_700-1000_Run_2_Year_2017.txt",
+	)
+
 	darkPhotonSignal = ("darkPhotonBaselineRun2.txt", )
 
 	darkPhotonNanoAOD = (
@@ -169,12 +196,15 @@ if __name__ == '__main__':
 	)
 
 	#background = ttBar + zz + dy + multiBoson + qcd # total 26 files
-	background = ttBar + zz + multiBoson + qcd # total 24 files - multiMuPtAnalysis coniguration
+	background = qcd
+	#background = ttBar + zz + multiBoson + qcd # total 24 files - multiMuPtAnalysis coniguration
+	#background = ttBar + zz + multiBoson + bQCD
 	#background = dy # just dy configuration
+	#background = bQCD
 
 	# List of jobs to run on from those above
 	# jobsList = [ttBar, zz, dy50, multiBoson, higgsSignal, higgsData] if analysis == 0 or analysis == 2 else [darkPhotonSignal]
-	jobsList = [higgsSignal] if analysis == 0 or analysis == 2 else [darkPhotonSignal] if analysis == 1 else [background] if analysis == 3 else [darkPhotonNanoAOD] if analysis == 4 else [dpData] if analysis == 6 else [higgsData]
+	jobsList = [higgsSignal] if analysis == 0 or analysis == 2 else [darkPhotonSignal] if analysis == 1 else [background] if analysis == 3 else [darkPhotonNanoAOD] if analysis == 4 else [dpData] if analysis == 6 else [plainQCD] if analysis == 7 else [higgsData]
 	
 	if os.path.exists("nohup.out") and (len(sys.argv) <= 2 or sys.argv[2] != "keep"):
 		os.remove("nohup.out")
