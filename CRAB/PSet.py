4f56ce1c24f87feaabefd5a541da0cdeb4efbe63
Vincent You <tingwenyou07@gmail.com>
2024-10-13 02:11:28 +0200

    initial CRAB stuff

---
+++ CRAB/PSet.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('NoSplit')
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+# process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch///store/mc/RunIISummer20UL17RECO/WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/AODSIM/106X_mc2017_realistic_v6-v1/230000/1883111F-FFE4-3D44-8AF0-865BB0C2BE4E.root'))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+# process.output = cms.OutputModule("PoolOutputModule",
+#     outputCommands = cms.untracked.vstring("drop *", "keep recoTracks_*_*_*"),
+#     fileName = cms.untracked.string('output.root'),
+# )
+# process.out = cms.EndPath(process.output)
---
+++ CRAB/runAnalyzer.sh
@@ -0,0 +1,8 @@
+cmsenv
+
+# Testing
+# find . -type f
+# echo $CMSSW_BASE
+# ls -a /
+
+runAnalyzer input=ZZ/ZZ_Decay_2e2mu_Run_2.txt output=test.root numFiles=3 analysis=HiggsBackground
