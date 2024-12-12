bfc82cc44e2002035cb5dabccadc6609dbfc5a85
Jayram Palamadai <bbobjoeyguy@gmail.com>
2024-12-12 12:56:10 -0600

    bdt training

---
+++ Analysis/src/HistVariable.cc
@@ -0,0 +1,16 @@
+#include "CMSAnalysis/Analysis/interface/HistVariable.hh"
+
+// Static variable definitions
+//pT, then some parameter -> switch for gensim/reco (EventInput.hh)
+HistVariable HistVariable::genSimSameSignMass("GenSim Same Sign Inv Mass");
+HistVariable HistVariable::sameSignMass("Same Sign Inv Mass");
+HistVariable HistVariable::invariantMass("Invariant Mass");
+HistVariable HistVariable::genSimPt("GenSim pT");
+HistVariable HistVariable::pt("pT");
+HistVariable HistVariable::eta("Eta");
+HistVariable HistVariable::phi("Phi");
+HistVariable HistVariable::mET("MET");
+HistVariable HistVariable::firstPt("firstPt");
+HistVariable HistVariable::secondPt("secondPt");
+HistVariable HistVariable::thirdPt("thirdPt");
+HistVariable HistVariable::fourthPt("fourthPt");
