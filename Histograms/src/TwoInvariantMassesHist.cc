19177e11018d19769edca63160910a05fd5dd743
KaleSG <jeremiahsuarez7267@gmail.com>
2024-12-16 09:37:14 -0600

    stash

--- Histograms/src/TwoInvariantMassesHist.cc
+++ Histograms/src/TwoInvariantMassesHist.cc
@@ -1,22 +1,77 @@
 #include "CMSAnalysis/Histograms/interface/TwoInvariantMassesHist.hh"
 #include "CMSAnalysis/Filters/interface/Selector.hh"
 #include "CMSAnalysis/Modules/interface/EventInput.hh"
+#include "CMSAnalysis/Utility/interface/ParticleCollection.hh"
+#include "CMSAnalysis/Utility/interface/Particle.hh"
+#include "CMSAnalysis/Utility/interface/ParticleType.hh"
+#include "TLorentzVector.h"
 
 TwoInvariantMassesHist::TwoInvariantMassesHist(const std::string &iname, int iNBinsX, int iNBinsY, double iMinX, double iMinY, double iMaxX, double iMaxY) : HistogramPrototype2D(iname, iNBinsX, iNBinsY, iMinX, iMinY, iMaxX, iMaxY){};
 
+double calculateInvariantMass(Particle particle1, Particle particle2) {
+  auto vec1 = particle1.getFourVector();
+  auto vec2 = particle2.getFourVector();
+
+  auto sum = vec1 + vec2;
+  return sum.M();
+
+}
+
+
 std::vector<std::pair<double, double>> TwoInvariantMassesHist::value2D() const
 {
-  auto invMasses = getInput()->getLeptons(EventInput::RecoLevel::Reco);
-  //->getCharge()
-  //->getType()
-  std::string filterString = getFilterString()
-  
+  auto Leptons = getInput()->getLeptons(EventInput::RecoLevel::Reco);
 
-  if (invMasses.size() != 2)
-  {
-    return {};
-    // std::runtime_error("Incorrect number of invariant masses calculated!");
+  if (Leptons.size() != 4) return {};
+
+  //Collection Initialize
+  ParticleCollection positiveLeptons;
+  ParticleCollection negativeLeptons;
+
+  //Sorts Lepton Pairs
+  for (auto lepton : Leptons) {
+    if (lepton.getCharge() == 1) positiveLeptons.addParticle(lepton);
+    if (lepton.getCharge() ==-1) negativeLeptons.addParticle(lepton);
   }
 
-  return {{invMasses[0], invMasses[1]}};
+  //Calculate Same Sign Invariant Masses
+  double positivePairInvariantMass = calculateInvariantMass(positiveLeptons[0],positiveLeptons[1]);
+  double negativePairInvariantMass = calculateInvariantMass(negativeLeptons[0],negativeLeptons[1]);
+
+  //Pair Combos
+  /* 
+  eeee + -
+  eeeu 
+  eeuu
+  eueu + -
+  euuu
+  uuuu + -
+  */
+
+  
+  int posElectronCount = 0;
+  int negElectronCount = 0;
+
+
+  //this can probably be better optomized but i dont feel like dealing with it right now (kale 12/11/24)
+  if (positiveLeptons[0].getType() == ParticleType::electron()) ++posElectronCount;
+  if (positiveLeptons[1].getType() == ParticleType::electron()) ++posElectronCount;
+  if (negativeLeptons[0].getType() == ParticleType::electron()) ++negElectronCount;
+  if (negativeLeptons[1].getType() == ParticleType::electron()) ++negElectronCount;
+
+  //Check if opposite sign particle pairs are the same leptons
+  if (posElectronCount == negElectronCount)
+    return {{positivePairInvariantMass,negativePairInvariantMass}};
+  
+  //Check which pair has more electrons if not equal
+  if (posElectronCount > negElectronCount) {
+    return {{positivePairInvariantMass,negativePairInvariantMass}};
+  } 
+  else 
+  {
+    return {{negativePairInvariantMass,positivePairInvariantMass}};
+  }
+ 
+    
+ 
 };
