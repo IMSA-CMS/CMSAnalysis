5a1593d6523aedaf29f66e776b6b5bd8460caacc
fCameron Eddington <ceddington@imsa.edu>
2025-05-15 13:43:38 -0500

    Hist name stuff

--- Analysis/interface/HistNameFinder.hh
+++ Analysis/interface/HistNameFinder.hh
@@ -3,12 +3,14 @@
 
 #include <string>
 #include "CMSAnalysis/Analysis/interface/HistVariable.hh"
+#include "CMSAnalysis/Utility/interface/ScaleFactor.hh"
+
 
 class HistNameFinder
 {
 public: //add systematic type
-    virtual std::string getHistName(HistVariable histVariable, SystematicType type) const = 0;
-
+    virtual std::string getHistName(HistVariable histVariable, ScaleFactor::SystematicType type, std::string systematicName) const = 0;
+    virtual ~HistNameFinder() = default;
 };
 
 #endif // HISTNAMEFINDER_HH
--- Analysis/src/DarkPhotonHistNameFinder.cc
+++ Analysis/src/DarkPhotonHistNameFinder.cc
@@ -1,12 +1,12 @@
-#include "CMSAnalysis/Analysis/interface/HiggsHistNameFinder.hh"
+#include "CMSAnalysis/Analysis/interface/DarkPhotonHistNameFinder.hh"
 
-std::string HiggsHistNameFinder::getHistName(HistVariable histVariable, ScaleFactor::SystematicType type, std::string systematicName) const
+std::string DarkPhotonHistNameFinder::getHistName(HistVariable histVariable, ScaleFactor::SystematicType type, std::string systematicName) const
 {
-	std::string path = channel + "__hists/" + channel;
-	if (isData)
-	{
-		path += "_Pass";
-	}
+	std::string path = "__hists/";
+	// if (isData)
+	// {
+	// 	path += "_Pass";
+	// }
 	path += "_" + histVariable.getName();
 // Add systematic type
 	
