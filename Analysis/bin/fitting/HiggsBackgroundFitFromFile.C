0fdd8efafd4333fd8d282872656a9510af3b9bd5
Brooke Hobbs <bhobbs@imsa.edu>
2025-09-17 13:56:45 -0500

    Paramaterization Histograms for Higgs

--- Analysis/bin/fitting/HiggsBackgroundFitFromFile.C
+++ Analysis/bin/fitting/HiggsBackgroundFitFromFile.C
@@ -49,9 +49,10 @@ std::vector<std::string> histogramTypes =
 void HiggsBackgroundFitFromFile()
 {
 	std::cout << "[INFO] Starting HiggsBackgroundFitFromFile()" << std::endl;
-	// const double min = 200;
-	// const double max = 2000;
-
+	//const double min = 200;
+	//const double max = 2000;
+	
+	//Output in OtherBackgroundFits
 	std::string fitHistsName = "OtherBackgroundFits.root";
 	std::string fitParameterValueFile = "OtherBackgroundFunctions.txt";
 
@@ -78,29 +79,93 @@ void HiggsBackgroundFitFromFile()
 	// 	// "WZZ",
 	// 	"t#bar{t}, Multiboson Background",
 	// };
-
 	std::map<std::string, std::pair<int, int>> backgroundsToRange = {
 		{"Drell-Yan Background", {140, 500}},
-		{"QCD Background", {200, 2000}}, // no events anyway
-		{"ZZ Background", {400, 800}},
-		{"TTbar Background", {70, 2000}},
-		// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
-		// {"TTZ Background", {120, 2000}},
-		// {"ZZZ Background", {200, 2000}}, // not enough to fit
-		// {"WW Background", {200, 2000}}, // not enough to fit
-		// {"WWW Background", {200, 2000}}, // not enough to fit
-		// {"WWZ Background", {200, 2000}}, // not enough
-		// {"WZ Background", {200, 2000}}, // not enough
-		// {"WZZ Background", {200, 2000}}, // enough on some?
-		{"Other Background", {200, 2000}},
+			{"QCD Background", {200, 2000}}, // no events anyway
+			{"ZZ Background", {200, 800}},
+			{"TTbar Background", {200, 2000}},
+			// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
+			// {"TTZ Background", {120, 2000}},
+			// {"ZZZ Background", {200, 2000}}, // not enough to fit
+			// {"WW Background", {200, 2000}}, // not enough to fit
+			// {"WWW Background", {200, 2000}}, // not enough to fit
+			// {"WWZ Background", {200, 2000}}, // not enough
+			// {"WZ Background", {200, 2000}}, // not enough
+			// {"WZZ Background", {200, 2000}}, // enough on some?
+			{"Other Background", {275, 900}}
 	};
+
+	std::map<std::string, std::map<std::string, std::pair<int, int>>> histRanges = {
+		{"h_mll1_eeee", {
+			{"Drell-Yan Background", {140, 500}},
+			{"QCD Background", {200, 2000}}, // no events anyway
+			{"ZZ Background", {300, 950}},
+			{"TTbar Background", {175, 750}},
+			// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
+			// {"TTZ Background", {120, 2000}},
+			// {"ZZZ Background", {200, 2000}}, // not enough to fit
+			// {"WW Background", {200, 2000}}, // not enough to fit
+			// {"WWW Background", {200, 2000}}, // not enough to fit
+			// {"WWZ Background", {200, 2000}}, // not enough
+			// {"WZ Background", {200, 2000}}, // not enough
+			// {"WZZ Background", {200, 2000}}, // enough on some?
+			{"Other Background", {320, 900}},
+			}},
+		{"h_mll1_mmmm", {
+			{"Drell-Yan Background", {140, 500}},
+			{"QCD Background", {200, 2000}}, // no events anyway
+			{"ZZ Background", {325, 900}},
+			{"TTbar Background", {200, 900}},
+			// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
+			// {"TTZ Background", {120, 2000}},
+			// {"ZZZ Background", {200, 2000}}, // not enough to fit
+			// {"WW Background", {200, 2000}}, // not enough to fit
+			// {"WWW Background", {200, 2000}}, // not enough to fit
+			// {"WWZ Background", {200, 2000}}, // not enough
+			// {"WZ Background", {200, 2000}}, // not enough
+			// {"WZZ Background", {200, 2000}}, // enough on some?
+			{"Other Background", {250, 800}},
+			}},
+		{"h_mll2_eeee", {
+			{"Drell-Yan Background", {140, 500}},
+			{"QCD Background", {200, 2000}}, // no events anyway
+			{"ZZ Background", {210, 900}},
+			{"TTbar Background", {110, 500}},
+			// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
+			// {"TTZ Background", {120, 2000}},
+			// {"ZZZ Background", {200, 2000}}, // not enough to fit
+			// {"WW Background", {200, 2000}}, // not enough to fit
+			// {"WWW Background", {200, 2000}}, // not enough to fit
+			// {"WWZ Background", {200, 2000}}, // not enough
+			// {"WZ Background", {200, 2000}}, // not enough
+			// {"WZZ Background", {200, 2000}}, // enough on some?
+			{"Other Background", {140, 550}},
+			}},
+		{"h_mll2_mmmm", {
+			{"Drell-Yan Background", {140, 500}},
+			{"QCD Background", {200, 2000}}, // no events anyway
+			{"ZZ Background", {175, 650}},
+			{"TTbar Background", {125,620}},
+			// {"TTW Background", {90, 2000}}, // almost no events on uuuu, figure out something
+			// {"TTZ Background", {120, 2000}},
+			// {"ZZZ Background", {200, 2000}}, // not enough to fit
+			// {"WW Background", {200, 2000}}, // not enough to fit
+			// {"WWW Background", {200, 2000}}, // not enough to fit
+			// {"WWZ Background", {200, 2000}}, // not enough
+			// {"WZ Background", {200, 2000}}, // not enough
+			// {"WZZ Background", {200, 2000}}, // enough on some?
+			{"Other Background", {175, 500}},
+			}}
+
+	
+};
 	std::map<std::string, std::vector<std::string>> fileMap =
 	{
 		{"Drell-Yan Background", {"DYJetsToLLM10to50", "DYJetsToLLM50"}},
 		{"QCD Background", {"QCD_HT1000to1500", "QCD_HT100to200", "QCD_HT1500to2000", "QCD_HT2000toInf", "QCD_HT200to300", "QCD_HT300to500", "QCD_HT500to700", "QCD_HT50to100", "QCD_HT700to1000"}},
 		{"ZZ Background", {"ZZTo2L2Nu", "ZZTo2Q2L", "ZZTo4L"}},
-		{"TTbar Background", {"ST_s-channel", "ST_t-channel_antitop", "ST_t-channel_top", "ST_tW_antitop", "ST_tW_top", "TTTo2L2Nu", "TTToHadronic", "TTToSemiLeptonic"}},
-		{"Other Background", {"GluGluZH", "HZJ_HToWWTo2L2Nu_ZTo2L", "TWZToLL", "WGToLNuG", "WJetsToLNu_0J", "WJetsToLNu_2J", "WJetsToLNu_2J", "WJetsToLNu_HT-100To200", "WJetsToLNu_HT-200To400", "WJetsToLNu_HT-400To600", "WJetsToLNu_HT-600To800", "WJetsToLNu_HT-70To100", "WJetsToLNu_HT-800To1200", "WJetsToLNu_NLO", "WWTo2L2Nu", "WWW", "WWZ", "WW", "WZTo2Q2L", "WZTo3LNu", "WZZ", "WZ", "ZHToMuMu", "ZHToTauTau", "ZZTo2L2Nu", "ZZTo2Q2L", "ZZTo4L", "ZZZ", "ttHJetToNonbb", "ttHTo2L2Nu", "ttHToEE", "ttHToTauTau", "ttWJets", "ttZJets"}}
+		{"TTbar Background", {"ST_s-channel", "ST_t-channel_antitop", "ST_t-channel_top", "ST_tW_antitop", "ST_tW_top", "TTTo2L2Nu", "TTToHadronic", "TTToSemiLeptonic", "ttHJetToNonbb", "ttHTo2L2Nu", "ttHToEE", "ttHToTauTau", "ttWJets", "ttZJets"}},
+		{"Other Background", {"GluGluZH", "HZJ_HToWWTo2L2Nu_ZTo2L", "TWZToLL", "WGToLNuG", "WJetsToLNu_0J", "WJetsToLNu_2J", "WJetsToLNu_2J", "WJetsToLNu_HT-100To200", "WJetsToLNu_HT-200To400", "WJetsToLNu_HT-400To600", "WJetsToLNu_HT-600To800", "WJetsToLNu_HT-70To100", "WJetsToLNu_HT-800To1200", "WJetsToLNu_NLO", "WWTo2L2Nu", "WWW", "WWZ", "WW", "WZTo2Q2L", "WZTo3LNu", "WZZ", "WZ", "ZHToMuMu", "ZHToTauTau", "ZZZ"}}
 
 	};
 	Fitter fitter(fitHistsName, fitParameterValueFile, parameterFits, parameterFunctions);
@@ -118,7 +183,8 @@ void HiggsBackgroundFitFromFile()
 			for (auto fileList : fileMap)
 			{
 				auto background = fileList.first;
-				auto range = backgroundsToRange[background];
+				std::string rangeKey = histType + "_" + channel;
+				auto range = histRanges[rangeKey][background];
 				TH1* selectedHist = combineHists(fileList.second, channel, histType);
 				if(selectedHist->GetEntries() < 1) continue;
 				std::string keyName = channel + '/' + background + " " + histType;
@@ -172,4 +238,4 @@ TH1* combineHists (std::vector<std::string> fileNames, std::string channel, std:
 		}
 	}
 	return hist;
-}
+};
