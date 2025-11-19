b002acabe0cdfd7990e281f95136d81a003f7ff4
Shiv Rai <srai@imsa.edu>
2025-11-19 14:33:34 -0600

    no change

--- Analysis/bin/SuperPlot.C
+++ Analysis/bin/SuperPlot.C
@@ -120,9 +120,9 @@ void SuperPlot()
 	// auto ana = std::make_shared<HiggsCompleteAnalysis>();
 	// TCanvas *canvas = plotFormatter->completePlot(ana, nameVar, xAxisTitle, yAxisTitle, false, true, false, "eeee", "Higgs Group 1000");
 	// //HistVariable nameVar(ParticleType::muon(), 0, HistVariable::VariableType::SameSignInvariantMass);
-	HistVariable nameVar(HistVariable::VariableType::RecoOppositeSignInvariantMass);
+	HistVariable nameVar(HistVariable::VariableType::RecoSameSignInvariantMass);
 	auto ana = std::make_shared<HiggsCompleteAnalysis>();
-	TCanvas *canvas = plotFormatter->completePlot(ana, nameVar, xAxisTitle, yAxisTitle, false, false, true, "e_e__ZPeak");
+	TCanvas *canvas = plotFormatter->completePlot(ana, nameVar, xAxisTitle, yAxisTitle, false, false, true, "ee___ZPeak");
 
 	// auto ana = std::make_shared<DarkPhotonCompleteAnalysis>();
 	// TCanvas *canvas = plotFormatter->completePlot(ana, nameVar, xAxisTitle, yAxisTitle, false, false, true, "High Mass and Different Sign/1Jet");
